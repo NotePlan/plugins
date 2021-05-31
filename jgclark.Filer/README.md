badd821212b50410784dd7601afb63bb4637acb9
jgclark <jonathan@clarksonline.me.uk>
2021-05-31 22:40:56 +0100

    Filer v0.3.0
    - add indented lines
    - add backlink date refs

--- jgclark.Filer/README.md
+++ jgclark.Filer/README.md
@@ -6,13 +6,18 @@ It works out what you want moving from the current open note using this priority
 - current selected region (you don't need to select the whole of the first or last lines; the plugin will grab the whole lines)
 - current heading and its following section (up to the next title of the same level or higher)
 - current line the cursor is in
+- current line the cursor is in, plus any indented lines that follow it
 
 It pops up the command bar to choose the project note you want to move it to, followed by the heading within that note to move it after.  You can press Escape (on Mac) at any time to cancel.  The move happens in the background, leaving you in the current note.
 
 ## Configuration
-None needed.
+If you want to stop date backlinks being added change the line at the top of the plugin'\ `fileItems.js` file to: `const pref_addDateBacklink = false;`
 
 ## History
+### v0.3.0, 31.5.2021
+- added ability to move any indented paragraphs after the selected line
+- creates a `>date` backlink when moving from a calendar note (requested by @Dimitry). Can be turned off by the `pref_addDateBacklink` setting (see above).
+
 ### v0.2.2, 26.5.2021
 - add ability to move paragraphs to top or bottom of note. (Top of note comes after title if there is one.)
 - works when moving to notes with _no title or headings at all_ [Issue 10 by @dwertheimer ]
--- jgclark.Filer/fileItems.js
+++ jgclark.Filer/fileItems.js
@@ -2,12 +2,18 @@
 // -----------------------------------------------------------------------------
 // Plugin to help move selected pargraphs to other notes
 // Jonathan Clark
-// v0.2.2, 26.5.2021
+// v0.3.0, 31.5.2021
 // -----------------------------------------------------------------------------
 
+// Preference that needs to get added when there is a proper config system
+const pref_addDateBacklink = true;
+
+// -----------------------------------------------------------------------------
+// Helper Functions
+
+// Return list of all project notes, sorted by changed date (newest to oldest)
 function projectNotesSortedByChanged() {
   const projectNotes = DataStore.projectNotes.slice();
-
   return projectNotes.sort(
     (first, second) =>
       first.changedDate.valueOf() - second.changedDate.valueOf(),
@@ -48,12 +54,16 @@ function parasToText(paras) {
   return parasAsText;
 }
 
+// -----------------------------------------------------------------------------
+
 async function fileParas() {
   // identify out what we're moving (in priority order):
   // - current selection
   // - current heading + its following section
   // - current line
-  // - TODO: current line (plus any indented paragraphs)
+  // - current line (plus any indented paragraphs)
+  // allow setting to create a backlink when moving from a calendar note (requested by @Dimitry)
+
   const { content, selectedParagraphs, note } = Editor;
   if (content == null || selectedParagraphs == null || note == null) {
     // No note open, or no paragraph selection (perhaps empty note), so don't do anything.
@@ -66,7 +76,6 @@ async function fileParas() {
     return;
   }
   const range = Editor.paragraphRangeAtCharacterIndex(selection.start);
-  // const firstSelPara = selectedParagraphs[0]; // needed?
   console.log('\nfileParse: selection ' + rangeToString(range));
 
   // Work out what paragraph number this selected para is
@@ -90,12 +99,9 @@ async function fileParas() {
     const para = selectedParagraphs[0];
     // paraDetails(para)
     console.log(
-      "  Para '" +
-      para.content +
-      "' type: " +
-      para.type +
-      ', index: ' +
-      firstSelParaIndex,
+      "  Para '" + para.content +
+      "' type: " + para.type +
+      ", index: " + firstSelParaIndex,
     );
     // if this is a heading, find the rest of the sections
     if (para.type === 'title') {
@@ -113,12 +119,28 @@ async function fileParas() {
       }
       console.log('  Found ' + parasToMove.length + ' heading section lines');
     } else {
-      // this isn't a heading. Now see if there are following indented lines
-      console.log('  Found single line with indent level ' + para.indents);
+      // This isn't a heading. 
+      // Now see if there are following indented lines to move as well
+      startingIndentLevel = para.indents;
+      console.log('  Found single line with indent level ' + startingIndentLevel);
       parasToMove.push(para);
-      // TODO following indented lines
+      for (let i = firstSelParaIndex + 1; i < allParas.length; i++) {
+        const p = allParas[i];
+        if (p.indents <= startingIndentLevel) {
+          // stop as this para is same or less indented than the starting line
+          break;
+        }
+        parasToMove.push(p);
+      }
       console.log('  Found ' + parasToMove.length + ' indented paras');
     }
+  }
+
+  // If this is a calendar note we've moving from, and the user wants to
+  // create a date backlink, then append backlink to the first para in parasToMove
+  if (pref_templateName && note.type === "Calendar") {
+    const todaysDate = new Date().toISOString().slice(0, 10)
+    parasToMove[0].content = parasToMove[0].content + ' >' + todaysDate
   }
 
   // There's no API function to work on multiple paragraphs,
@@ -140,7 +162,6 @@ async function fileParas() {
   let headingStrings = []
   const headingParas = noteToMoveTo.paragraphs.filter((p) => p.type === 'title');  // = all headings, not just the top 'title'
   // console.log(headingParas.length);
-  // paraDetails(headingParas[0]);
   if (headingParas.length > 0) {
     headingStrings = headingParas.map((p) => {
       let prefix = "";
@@ -163,6 +184,7 @@ async function fileParas() {
   const headingToFind = headingStrings[res.index].trim();
   console.log('    under heading: ' + headingToFind);
 
+  
   // Add to new location
   // Currently there's no API function to deal with multiple paragraphs, but we can
   // insert a raw text string
