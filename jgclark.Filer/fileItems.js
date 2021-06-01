1c86c9da10e1e46de2249ca4271bad8f15e8883b
Naman Goel <naman34@gmail.com>
2021-06-01 02:53:12 -0700

    Fix problems.

--- jgclark.Filer/fileItems.js
+++ jgclark.Filer/fileItems.js
@@ -6,6 +6,7 @@
 // -----------------------------------------------------------------------------
 
 // Preference that needs to get added when there is a proper config system
+// eslint-disable-next-line no-unused-vars
 const pref_addDateBacklink = true
 
 // -----------------------------------------------------------------------------
@@ -23,38 +24,38 @@ function projectNotesSortedByChanged() {
 
 // Pretty print range information
 function rangeToString(r) {
-  if (r == undefined) {
+  if (r == null) {
     return 'Range is undefined!'
   }
-  return 'range: ' + r.start + '-' + r.end
+  return `range: ${r.start}-${r.end}`
 }
 
 // Print out paragraph details
 // Currently unused
-function paraDetails(p) {
-  console.log(
-    'Para content: ' +
-      p.content +
-      '\n\trawContent: ' +
-      p.rawContent +
-      '\n\tlineIndex: ' +
-      p.lineIndex +
-      '\ttype: ' +
-      p.type +
-      '\tindents: ' +
-      p.indents + // NB only counts tabs not spaces
-      '\tprefix: ' +
-      p.prefix,
-  )
-  if (p.headingRange != undefined) {
-    console.log(
-      '\theadingRange from ' +
-        p.headingRange.start +
-        ' len ' +
-        p.headingRange.length,
-    )
-  }
-}
+// function paraDetails(p) {
+//   console.log(
+//     'Para content: ' +
+//       p.content +
+//       '\n\trawContent: ' +
+//       p.rawContent +
+//       '\n\tlineIndex: ' +
+//       p.lineIndex +
+//       '\ttype: ' +
+//       p.type +
+//       '\tindents: ' +
+//       p.indents + // NB only counts tabs not spaces
+//       '\tprefix: ' +
+//       p.prefix,
+//   )
+//   if (p.headingRange != undefined) {
+//     console.log(
+//       '\theadingRange from ' +
+//         p.headingRange.start +
+//         ' len ' +
+//         p.headingRange.length,
+//     )
+//   }
+// }
 
 // Convert paragraph(s) to single raw text string
 function parasToText(paras) {
@@ -63,7 +64,7 @@ function parasToText(paras) {
   for (let i = 0; i < paras.length; i++) {
     const p = paras[i]
     // paraDetails(p)
-    text += p.rawContent + '\n'
+    text += `${p.rawContent}\n`
   }
   const parasAsText = text.trimEnd() // remove extra newline not wanted after last line
   return parasAsText
@@ -90,7 +91,7 @@ async function fileParas() {
   }
   const range = Editor.paragraphRangeAtCharacterIndex(selection.start)
   // const firstSelPara = selectedParagraphs[0]; // needed?
-  console.log('\nfileParse: selection ' + rangeToString(range))
+  console.log(`\nfileParse: selection ${rangeToString(range)}`)
 
   // Work out what paragraph number this selected para is
   let firstSelParaIndex = 0
@@ -101,30 +102,25 @@ async function fileParas() {
       break
     }
   }
-  console.log('  First para index: ' + firstSelParaIndex)
+  console.log(`  First para index: ${firstSelParaIndex}`)
 
   let parasToMove = []
   if (selectedParagraphs.length > 1) {
     // we have a selection of paragraphs, so use them
     parasToMove = selectedParagraphs
-    console.log('  Found ' + parasToMove.length + ' selected paras')
+    console.log(`  Found ${parasToMove.length} selected paras`)
   } else {
     // we have just one paragraph selected -- the current one
     const para = selectedParagraphs[0]
     // paraDetails(para)
     console.log(
-      "  Para '" +
-        para.content +
-        "' type: " +
-        para.type +
-        ', index: ' +
-        firstSelParaIndex,
+      `  Para '${para.content}' type: ${para.type}, index: ${firstSelParaIndex}`,
     )
     // if this is a heading, find the rest of the sections
     if (para.type === 'title') {
       // includes all heading levels
       const thisHeadingLevel = para.headingLevel
-      console.log('  Found heading level ' + thisHeadingLevel)
+      console.log(`  Found heading level ${thisHeadingLevel}`)
       parasToMove.push(para) // make this the first line to move
       // Work out how far this section extends. (NB: headingRange doesn't help us here.)
       for (let i = firstSelParaIndex + 1; i < allParas.length; i++) {
@@ -134,13 +130,13 @@ async function fileParas() {
         } // stop as new heading of same or higher level
         parasToMove.push(p)
       }
-      console.log('  Found ' + parasToMove.length + ' heading section lines')
+      console.log(`  Found ${parasToMove.length} heading section lines`)
     } else {
       // This isn't a heading.
       // Now see if there are following indented lines to move as well
-      startingIndentLevel = para.indents
+      const startingIndentLevel = para.indents
       console.log(
-        '  Found single line with indent level ' + startingIndentLevel,
+        `  Found single line with indent level ${startingIndentLevel}`,
       )
       parasToMove.push(para)
       for (let i = firstSelParaIndex + 1; i < allParas.length; i++) {
@@ -151,15 +147,15 @@ async function fileParas() {
         }
         parasToMove.push(p)
       }
-      console.log('  Found ' + parasToMove.length + ' indented paras')
+      console.log(`  Found ${parasToMove.length} indented paras`)
     }
   }
 
   // If this is a calendar note we've moving from, and the user wants to
   // create a date backlink, then append backlink to the first para in parasToMove
-  if (pref_templateName && note.type === 'Calendar') {
+  if (note.type === 'Calendar') {
     const todaysDate = new Date().toISOString().slice(0, 10)
-    parasToMove[0].content = parasToMove[0].content + ' >' + todaysDate
+    parasToMove[0].content = `${parasToMove[0].content} >${todaysDate}`
   }
 
   // There's no API function to work on multiple paragraphs,
@@ -172,10 +168,10 @@ async function fileParas() {
   const notes = projectNotesSortedByChanged()
   let res = await CommandBar.showOptions(
     notes.map((n) => n.title ?? 'null'),
-    'Select project note to move ' + parasToMove.length + ' to',
+    `Select project note to move ${parasToMove.length} to`,
   )
   const noteToMoveTo = notes[res.index]
-  console.log('  Moving to note: ' + (noteToMoveTo.title ?? 'Untitled'))
+  console.log(`  Moving to note: ${noteToMoveTo.title ?? 'Untitled'}`)
 
   // ask to which heading to add the paras
   let headingStrings = []
@@ -184,7 +180,7 @@ async function fileParas() {
   if (headingParas.length > 0) {
     headingStrings = headingParas.map((p) => {
       let prefix = ''
-      for (var i = 1; i < p.headingLevel; i++) {
+      for (let i = 1; i < p.headingLevel; i++) {
         prefix += '    '
       }
       return prefix + p.content
@@ -198,12 +194,12 @@ async function fileParas() {
   headingStrings.push('(bottom of note)') // add at end
   res = await CommandBar.showOptions(
     headingStrings,
-    "Select a heading from note '" +
-      (noteToMoveTo.title ?? 'Untitled') +
-      "' to move after",
+    `Select a heading from note '${
+      noteToMoveTo.title ?? 'Untitled'
+    }' to move after`,
   )
   const headingToFind = headingStrings[res.index].trim()
-  console.log('    under heading: ' + headingToFind)
+  console.log(`    under heading: ${headingToFind}`)
 
   // Add to new location
   // Currently there's no API function to deal with multiple paragraphs, but we can
@@ -212,14 +208,14 @@ async function fileParas() {
   // note.addParagraphBelowHeadingTitle(parasToMove, 'empty', heading.content, false, false);
   const destParas = noteToMoveTo.paragraphs
   let insertionIndex = null
-  if (headingToFind == '(top of note)') {
+  if (headingToFind === '(top of note)') {
     insertionIndex = 0
-  } else if (headingToFind == '(bottom of note)') {
+  } else if (headingToFind === '(bottom of note)') {
     insertionIndex = destParas.length + 1
   } else {
     for (let i = 0; i < destParas.length; i++) {
       const p = destParas[i]
-      if (p.content == headingToFind && p.type === 'title') {
+      if (p.content === headingToFind && p.type === 'title') {
         insertionIndex = i + 1
         break
       }
@@ -228,7 +224,7 @@ async function fileParas() {
   if (insertionIndex == null) {
     return
   }
-  console.log('  Inserting at index ' + insertionIndex)
+  console.log(`  Inserting at index ${insertionIndex}`)
   await noteToMoveTo.insertParagraph(parasAsText, insertionIndex, 'empty')
 
   // delete from existing location
@@ -239,7 +235,7 @@ async function fileParas() {
     i >= firstSelParaIndex;
     i--
   ) {
-    console.log('  Remove original para # ' + i)
+    console.log(`  Remove original para # ${i}`)
     note.removeParagraphAtIndex(i)
   }
 }
