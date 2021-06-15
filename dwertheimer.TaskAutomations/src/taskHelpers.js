9674fa92f9fc1b107ee198575da3babf69dfaef0
David Wertheimer <dwertheimer@users.noreply.github.com>
2021-06-15 16:28:13 -0700

    Extracting elements (#@!) from tasks

--- dwertheimer.TaskAutomations/src/taskHelpers.js
+++ dwertheimer.TaskAutomations/src/taskHelpers.js
@@ -1,4 +1,22 @@
+/* eslint-disable max-len */
 import { hyphenatedDateString } from './dateHelpers'
+
+const HASHTAGS = /\B#([a-zA-Z0-9]+\b)/g
+const MENTIONS = /\B@([a-zA-Z0-9]+\b)/g
+const EXCLAMATIONS = /\B(!+\B)/g
+const PARENS_PRIORITY = /^\s*(\([a-zA-z]\))\B/g // must be at start of content
+
+function getElementsFromTask(content, reSearch) {
+  const found = []
+  let matches = reSearch.exec(content)
+
+  do {
+    if (matches?.length > 1) {
+      found.push(matches[1].trim())
+    }
+  } while ((matches = reSearch.exec(content)) !== null)
+  return found
+}
 
 export function getOverdueTasks(paragraphs) {
   const todayDateString = hyphenatedDateString(new Date())
@@ -13,17 +31,39 @@ export function getOverdueTasks(paragraphs) {
   // For now, we are assuming that sweep was already done, and we're just looking at this one note
 }
 
+/*
+ * @param Paragraphs array
+ * @return tasks object of tasks by type {'open':[], 'scheduled'[], 'done':[], 'cancelled':[]}
+ */
 export function getTasksByType(paragraphs) {
   const tasks = {}
   // * @type {"open", "done", "scheduled", "cancelled", "title", "quote", "list" (= bullet), "empty" (no content) or "text" (= plain text)}
-  const taskTypes = ['open', 'done', 'scheduled', 'cancelled']
+  const taskTypes = ['open', 'scheduled', 'done', 'cancelled']
   taskTypes.forEach((t) => (tasks[t] = []))
   paragraphs.forEach((para, index) => {
     if (taskTypes.indexOf(para.type) >= 0) {
-      console.log(`\t${para.type}: ${para.content}`)
-      tasks[para.type].push({ content: para.content, index })
+      const content = para.content
+      console.log(`\t${index}: ${para.type}: ${para.content}`)
+      try {
+        const hashtags = getElementsFromTask(content, HASHTAGS)
+        const mentions = getElementsFromTask(content, MENTIONS)
+        const exclamations = getElementsFromTask(content, EXCLAMATIONS)
+        const parensPriority = getElementsFromTask(content, PARENS_PRIORITY)
+
+        tasks[para.type].push({
+          content: para.content,
+          index,
+          raw: para.rawContent,
+          hashtags,
+          mentions,
+          exclamations,
+          parensPriority,
+        })
+      } catch (error) {
+        console.log(error, para.content, index)
+      }
     } else {
-      // console.log(`\t\tSkip: ${para.content}`)
+      // console.log(`\t\tSkip: ${para.content}`) //not a task
     }
   })
   console.log(`\t${JSON.stringify(tasks)}`)
