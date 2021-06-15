7d2448ea433f21ceb7f472d1bbf7785a07a59342
David Wertheimer <dwertheimer@users.noreply.github.com>
2021-06-15 06:54:02 -0700

    Initial Commit

---
+++ dwertheimer.TaskAutomations/script.js
@@ -0,0 +1,18 @@
+var exports = (function (exports) {
+  'use strict';
+
+  // Type checking reference: https://flow.org/
+  // Specific how-to re: Noteplan: https://github.com/NotePlan/plugins/blob/main/Flow_Guide.md
+  async function sortTasks() {
+    // write code here
+    console.log('Hello World');
+  }
+
+  exports.sortTasks = sortTasks;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+  return exports;
+
+}({}));
+Object.assign(globalThis, exports)
---
+++ dwertheimer.TaskAutomations/src/sortTasks.js
@@ -0,0 +1,7 @@
+// @flow
+// Type checking reference: https://flow.org/
+// Specific how-to re: Noteplan: https://github.com/NotePlan/plugins/blob/main/Flow_Guide.md
+export default async function sortTasks(): Promise<void> {
+  // write code here
+  console.log('Hello World')
+}
