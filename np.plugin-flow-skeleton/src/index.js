97c6a8c01868036b7be0cc67850a915ed4b66073
David Wertheimer <dwertheimer@users.noreply.github.com>
2021-08-06 11:51:52 -0700

    Updates to the skeleton, adding changelog template

--- np.plugin-flow-skeleton/src/index.js
+++ np.plugin-flow-skeleton/src/index.js
@@ -4,6 +4,10 @@
 // The beauty of this set-up is that each NP command can have its own file
 // And all will be packaged together into one file for NP to load
 // from Terminal: npm run autowatch (should watch and re-bundle every time you edit)
+// `npm run autowatch` will watch for changes and will compile the Plugin script code
+// and copy it to your plugins directory
+// Since NP reloads the Javascript every time you CMD-J to insert a plugin,
+// you can immmediately test the new code with NP
 // Add a line below for each function that you want NP to have access to.
 // Typically, listed below are only the top-level plug-in functions listed in plugin.json
 export { default as insertPluginFunctionNameHere } from './insertPluginFunctionNameHere'
--- np.plugin-flow-skeleton/src/insertPluginFunctionNameHere.js
+++ np.plugin-flow-skeleton/src/insertPluginFunctionNameHere.js
@@ -4,7 +4,12 @@
 // export default async function [name of the function called by Noteplan]
 // Type checking reference: https://flow.org/
 // Specific how-to re: Noteplan: https://github.com/NotePlan/plugins/blob/main/Flow_Guide.md
-export default async function insertPluginFunctionNameHere(): Promise<void> {
+export async function insertPluginFunctionNameHere(): Promise<void> {
   // write code here
-  console.log('Hello World')
+  console.log(
+    `Hello World. This text shows up in the Noteplan > Help > Plugin Console`,
+  )
+  Editor.insertTextAtCursor(
+    `This text shows up at the cursor in the note you're editing`,
+  )
 }
