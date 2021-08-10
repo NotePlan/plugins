8403c99e6df85aa86651c93a462602b7cb5ac264
Jonathan Clark <jonathan@clarksonline.me.uk>
2021-08-10 21:52:12 +0100

    Towards Review v0.3.0

--- jgclark.Review/src/index.js
+++ jgclark.Review/src/index.js
@@ -3,12 +3,16 @@
 //-----------------------------------------------------------------------------
 // Statistic commands for notes and projects
 // Jonathan Clark & Eduard Metzger
-// v0.2.3, 1.8.2021
+// v0.3.0, 9.8.2021
 //-----------------------------------------------------------------------------
 
 export {
-  projectLists, startReviews, nextReview, completeReview, completeProject,
+  projectLists, startReviews, nextReview, completeReview,
 } from './reviews'
+
+export {
+  addProject, completeProject,
+} from './projects'
 
 export {
   getOrMakeMetadataLine,
