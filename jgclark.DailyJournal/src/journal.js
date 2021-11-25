73534b3697c9bfb5e3ecd47895859f0b98094697
m1well <michaelwellner@gmx.de>
2021-11-25 21:24:16 +0100

    dailyjournal idea question improvements

--- jgclark.DailyJournal/src/journal.js
+++ jgclark.DailyJournal/src/journal.js
@@ -2,10 +2,10 @@
 //--------------------------------------------------------------------------------------------------------------------
 // Daily Journal plugin for NotePlan
 // Jonathan Clark
-// v0.8.2, 5.10.2021
+// v0.9.0, 25.11.2021
 //--------------------------------------------------------------------------------------------------------------------
 
-import { showMessage } from '../../helpers/userInput'
+import { isInt, showMessage } from '../../helpers/userInput'
 import { displayTitle } from '../../helpers/general'
 import { getOrMakeConfigurationSection } from '../../nmn.Templates/src/configuration'
 import { applyNamedTemplate } from '../../nmn.Templates/src/index'
@@ -24,18 +24,7 @@ const MINIMUM_JOURNAL_OPTIONS = {
 
 const pref_templateTitle = 'Daily Note Template' // fixed
 let pref_reviewSectionHeading: string
-let pref_moods: string
 let pref_moodArray: Array<string>
-
-//------------------------------------------------------------------
-// Helper functions
-
-// test for integer
-// taken from https://stackoverflow.com/questions/14636536/how-to-check-if-a-variable-is-an-integer-in-javascript
-function isInt(value) {
-  const x = parseFloat(value)
-  return !isNaN(value) && (x | 0) === x
-}
 
 //------------------------------------------------------------------
 // Main functions
@@ -45,8 +34,7 @@ export async function todayStart(): Promise<void> {
   try {
     await dayStart(true)
     await showMessage('Completed /todayStart')
-  }
-  catch (error) {
+  } catch (error) {
     await showMessage(error)
   }
 }
@@ -88,17 +76,17 @@ export async function dayReview(): Promise<void> {
   if (journalConfig == null
     || Object.keys(journalConfig).length === 0) // this is how to check for empty object
   {
-    console.log("\tWarning: Cannot find suitable 'dailyJournal' settings in Templates/_configuration note. Stopping.")
+    console.log('\tWarning: Cannot find suitable \'dailyJournal\' settings in Templates/_configuration note. Stopping.')
     await showMessage(
-      "Cannot find 'dailyJournal' settings in _configuration.",
-      "Yes, I'll check my _configuration settings.",
+      'Cannot find \'dailyJournal\' settings in _configuration.',
+      'Yes, I\'ll check my _configuration settings.',
     )
     return
   }
   // Finalise config settings
   const pref_reviewQuestions = (journalConfig?.reviewQuestions != null)
     ? String(journalConfig?.reviewQuestions)
-    : "@sleep(<number>)\\n@work(<number>)\\n@fruitveg(<int>)\\nMood:: <mood>\\nExercise:: <string>\\nGratitude:: <string>\\nGod was:: <string>\\nAlive:: <string>\\nNot Great:: <string>\\nWife:: <string>\\nRemember:: <string>"
+    : '@sleep(<number>)\\n@work(<number>)\\n@fruitveg(<int>)\\nMood:: <mood>\\nExercise:: <string>\\nGratitude:: <string>\\nGod was:: <string>\\nAlive:: <string>\\nNot Great:: <string>\\nWife:: <string>\\nRemember:: <string>'
   pref_reviewSectionHeading = (journalConfig?.reviewSectionHeading != null)
     ? String(journalConfig?.reviewSectionHeading)
     : 'Journal'
@@ -131,10 +119,10 @@ export async function dayReview(): Promise<void> {
   console.log(`\tFound ${numQs} question lines`)
   for (i = 0; i < numQs; i++) {
     // remove type indicators from the question string
-    question[i] = questionLines[i].replace(/:|\(|\)|<string>|<int>|<number>|<mood>/g, '').trim()
+    question[i] = questionLines[i].replace(/:|\(|\)|<string>|<int>|<number>|<mood>|<subheading>/g, '').trim()
     const reArray = questionLines[i].match(typeRE)
     questionType[i] = reArray?.[1] ?? '<error in question type>'
-    // console.log("\t" + i + ": " + question[i] + " / " + questionType[i])
+    // console.log('\t' + i + ': ' + question[i] + ' / ' + questionType[i])
   }
   // Ask each question in turn
   for (i = 0; i < numQs; i++) {
@@ -146,7 +134,11 @@ export async function dayReview(): Promise<void> {
       case 'int': {
         const reply = await CommandBar.showInput(questionType[i], `${question[i]}: %@`)
         if (reply != null && isInt(reply)) {
-          reviewLine = questionLines[i].replace(/<int>/, reply)
+          if (questionLines[i].startsWith('-')) {
+            reviewLine = `- ${reply}`
+          } else {
+            reviewLine = questionLines[i].replace(/<int>/, reply)
+          }
         } else {
           console.log(`\tERROR trying to get integer answer for question '${question[i]}'`)
         }
@@ -155,7 +147,11 @@ export async function dayReview(): Promise<void> {
       case 'number': {
         const reply = await CommandBar.showInput(questionType[i], `${question[i]}: %@`)
         if (reply != null && Number(reply)) {
-          reviewLine = questionLines[i].replace(/<number>/, reply)
+          if (questionLines[i].startsWith('-')) {
+            reviewLine = `- ${reply}`
+          } else {
+            reviewLine = questionLines[i].replace(/<number>/, reply)
+          }
         } else {
           console.log(`\tERROR trying to get number answer for question '${question[i]}'`)
         }
@@ -163,8 +159,12 @@ export async function dayReview(): Promise<void> {
       }
       case 'string': {
         const replyString = await CommandBar.showInput(questionType[i], `${question[i]}: %@`)
-        if (replyString != null) {
-          reviewLine = replyString !== '' ? questionLines[i].replace(/<string>/, replyString) : ''
+        if (replyString != null && replyString !== '') {
+          if (questionLines[i].startsWith('-')) {
+            reviewLine = `- ${replyString}`
+          } else {
+            reviewLine = replyString !== '' ? questionLines[i].replace(/<string>/, replyString) : ''
+          }
         } else {
           console.log(`\tERROR trying to get string answer for question '${question[i]}'`)
         }
@@ -174,10 +174,14 @@ export async function dayReview(): Promise<void> {
         const reply = await CommandBar.showOptions(pref_moodArray, 'Choose appropriate mood')
         const replyMood = pref_moodArray[reply.index]
         if (replyMood != null && replyMood !== '') {
-          reviewLine = questionLines[i].replace(/<mood>/, replyMood)
+          reviewLine = `**${questionLines[i].replace(/<mood>/, replyMood)}**`
         } else {
           console.log('\tERROR trying to get mood answer')
         }
+        break
+      }
+      case 'subheading': {
+        reviewLine = '\n### '.concat(question[i].replace(/<subheading>/, ''))
         break
       }
     }
