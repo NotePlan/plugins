9b6a7e4b92c1e64130a41c98460cd12a0d1f0887
David Wertheimer <dwertheimer@users.noreply.github.com>
2021-12-30 16:13:14 -0800

    Update insertionIndex code to deal with any type of note
    git shows lots of changes (I guess due to Lint config), but I only made one change (line 89)

--- helpers/paragraph.js
+++ helpers/paragraph.js
@@ -86,7 +86,7 @@ export function calcSmartPrependPoint(note: TNote): number {
   const lines = note.content?.split('\n') ?? ['']
 
   // By default we prepend at line 1, i.e. right after the Title line
-  let insertionLine = 1
+  let insertionLine = note.type === 'Calendar' ? 0 : 1
   // If we have any content, check for these special cases
   if (lines.length > 0) {
     if (lines[0] === '---') {
@@ -103,11 +103,7 @@ export function calcSmartPrependPoint(note: TNote): number {
       }
       if (insertionLine === 1) {
         // If we get here we haven't found an end to the YAML block.
-        console.log(
-          `Warning: couldn't find end of YAML frontmatter in note ${displayTitle(
-            note,
-          )}`,
-        )
+        console.log(`Warning: couldn't find end of YAML frontmatter in note ${displayTitle(note)}`)
         // It's not clear what to do at this point, so will leave insertion point as is
       }
     } else if (lines[1].match(/^#[A-z]/)) {
@@ -142,16 +138,12 @@ export function calcSmartPrependPoint(note: TNote): number {
  * @param {string} paraText - the text to prepend
  * @param {ParagraphType} paragraphType - the usual paragraph type to prepend
  */
-export function smartPrependPara(
-  note: TNote,
-  paraText: string,
-  paragraphType: ParagraphType,
-): void {
+export function smartPrependPara(note: TNote, paraText: string, paragraphType: ParagraphType): void {
   // Insert the text at the smarter insertionLine line
   note.insertParagraph(paraText, calcSmartPrependPoint(note), paragraphType)
 }
 
-// 
+//
 /**
  * Works out where the first ## Done or ## Cancelled section starts, if present.
  * If not, return the last paragraph index.
@@ -173,12 +165,7 @@ export function findEndOfActivePartOfNote(note: TNote): number {
       cancelledHeaderLine = i
     }
   }
-  const endOfActive =
-    (doneHeaderLine > 0)
-      ? doneHeaderLine
-      : ((cancelledHeaderLine > 0)
-        ? cancelledHeaderLine
-        : lineCount)
+  const endOfActive = doneHeaderLine > 0 ? doneHeaderLine : cancelledHeaderLine > 0 ? cancelledHeaderLine : lineCount
   // console.log(`  dHL = ${doneHeaderLine}, cHL = ${cancelledHeaderLine} endOfActive = ${endOfActive}`)
   return endOfActive
 }
