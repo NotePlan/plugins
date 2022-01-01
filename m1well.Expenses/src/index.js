facc7a173513dfd7bcfc234e857aedaf400834a5
m1well <michaelwellner@gmx.de>
2022-01-01 17:51:06 +0100

    expenses mini update

--- m1well.Expenses/src/index.js
+++ m1well.Expenses/src/index.js
@@ -3,7 +3,7 @@
 // -----------------------------------------------------------------------------
 // Plugin to store your expenses for further analyis
 // Michael Wellner (@m1well)
-// v1.4.0, 15.12.2021
+// v1.5.0, 01.01.2022
 // -----------------------------------------------------------------------------
 
 export { expensesTracking, expensesAggregate, individualTracking, shortcutsTracking, fixedTracking } from './expenses'
