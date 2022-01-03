f2537dcf3a6943a9aefe584068d9eb2eb3ae2258
jgclark <jonathan@clarksonline.me.uk>
2022-01-03 22:32:45 +0000

    NoteHelpers v0.10.7

--- jgclark.NoteHelpers/src/indexFolders.js
+++ jgclark.NoteHelpers/src/indexFolders.js
@@ -1,11 +1,14 @@
 // @flow
-//--------------------------------------------------------------------------------------------------------------------
-// Part of Note Helpers plugin for NotePlan
+//-----------------------------------------------------------------------------
 // Jonathan Clark
-// at v0.10.2, 30.7.2021
-//--------------------------------------------------------------------------------------------------------------------
+// Last updated 3.1.2022 for v0.10.7
+//-----------------------------------------------------------------------------
 
-import { nowShortDateTime } from '../../helpers/dateTime'
+import {
+  daysBetween,
+  nowLocaleDateTime,
+  relativeDateFromNumber,
+} from '../../helpers/dateTime'
 import { getFolderFromFilename } from '../../helpers/folders'
 import {
   defaultFileExt,
@@ -18,10 +21,16 @@ import {
   showMessage,
 } from '../../helpers/userInput'
 
-//-----------------------------------------------------------------
-// Command to calculate the index of a specified folder.
-// Input is folder name (without trailling /)
-// Returns an array of strings, one for each output line.
+//-----------------------------------------------------------------------------
+/** 
+ * Command to generate the index of a specified folder, including
+ * each note as a wikilink, with relative time since it was last updated.
+ * @author @jgclark
+ * 
+ * @param {string} folder - folder name (without trailling /)
+ * @param {boolean} includeSubfolders
+ * @return {[string]} array of strings, one for each output line
+*/
 function makeFolderIndex(
   folder: string,
   includeSubfolders: boolean,
@@ -32,7 +41,6 @@ function makeFolderIndex(
     } subfolders)`,
   )
 
-  let noteCount = 0
   const outputArray: Array<string> = []
   let folderList: Array<string> = []
   // if we want a to include any subfolders, create list of folders
@@ -43,49 +51,50 @@ function makeFolderIndex(
     folderList = [folder]
   }
   console.log(`\tFound ${folderList.length} matching folder(s)`)
+
   // Iterate over the folders
-  // A for-of loop is cleaner and less error prone than a regular for-loop
+  let subFolder = false
   for (const f of folderList) {
+    // Get list of the notes in this folder, but ignore any '_index' notes :-)
     const notes = notesInFolderSortedByTitle(f)
-    // console.log(notes.length)
+      .filter((n) => !n.title?.startsWith('_index'))
+    outputArray.push(subFolder ? `### ${f}` : `_index: ${f}\n_Index generated ${nowLocaleDateTime}. Times are since note was last updated._`)
     if (notes.length > 0) {
       // If this is a sub-folder level, then prefix with ### for a 3rd level heading,
       // otherwise leave blank, as a suitable header gets added elsewhere.
-      outputArray.push(noteCount > 0 ? `### ${f} Index` : `_index ${f}`)
-      outputArray.push(
-        `(${notes.length} notes, last updated: ${nowShortDateTime})`,
-      )
+      outputArray.push(`${notes.length} notes`)
       // iterate over this folder's notes
       for (const note of notes) {
-        outputArray.push(titleAsLink(note))
+        const relativeTimeSinceUpdate = relativeDateFromNumber(daysBetween(new Date(), note.changedDate))
+        outputArray.push(`${titleAsLink(note)} ${relativeTimeSinceUpdate}`)
       }
       outputArray.push('')
-      noteCount += notes.length
+    } else {
+      outputArray.push('(No notes found)')
     }
+    subFolder = true
   }
 
   return outputArray
 }
 
-//----------------------------------------------------------------
-// Command to index folders, creating list of note links
-// Options:
-// 1. This folder only (insert into current note)
-// 2. This folder only (add/update to _index note)
-// 3. This folder + subfolders (add/update into single _index note)
-// 4. TODO: This folder + subfolders (add/update into _index notes in each subfolder)
-
+/**
+ * Command to index folders, creating list of notes
+ * Options:
+ * 1. This folder only (insert into current note)
+ * 2. This folder only (add/update to _index note)
+ * 3. This folder + subfolders (add/update into single _index note)
+ * 4. TODO: This folder + subfolders (add/update into _index notes in each subfolder)
+ * @author @jgclark
+ */
 export async function indexFolders(): Promise<void> {
   // To start with just operate on current note's folder
   const fullFilename = Editor.filename ?? undefined
-  // const currentNote = Editor.note ?? undefined
   let thisFolder: string
   let outputArray: Array<string> = []
 
   if (fullFilename === undefined) {
-    console.log(
-      `  Info: No current filename (and therefore folder) found, so will ask instead.`,
-    )
+    console.log(`  Info: No current filename (and therefore folder) found, so will ask instead.`)
     thisFolder = await chooseFolder(`Please pick folder to index`)
   } else {
     thisFolder = getFolderFromFilename(fullFilename)
@@ -130,7 +139,6 @@ export async function indexFolders(): Promise<void> {
     outputArray = makeFolderIndex(thisFolder, true)
   }
   const outString = outputArray.join('\n')
-  console.log(`  -> ${outString}`)
 
   if (option.endsWith('index')) {
     // write out to index file(s)
@@ -162,5 +170,5 @@ export async function indexFolders(): Promise<void> {
     Editor.insertTextAtCursor(`${outString}`)
   }
 
-  console.log(`\nFinished indexFolders.`)
+  console.log(`Finished indexFolders.`)
 }
