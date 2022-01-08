d56fc1e8e64467f4f00dab98dbe53f26e4142272
David Wertheimer <dwertheimer@users.noreply.github.com>
2022-01-08 08:21:35 -0800

    [WIP] Made changes to flow defs in dev and shrunk the environment log

--- helpers/dev.js
+++ helpers/dev.js
@@ -1,51 +1,67 @@
 // Development-related helper functions
 
+// @flow
+
 /**
  * JSON.stringify() with support for Prototype properties
  * @author @dwertheimer
- * 
+ *
  * @param {object} obj
- * @param {boolean} prettyPrint - if true, will return a string with newlines and indentation
+ * @param {string | number} space - A String or Number of spaces that's used to insert white space (including indentation, line break characters, etc.) into the output JSON string for readability purposes.
  * @returns {string} stringified object
  * @example console.log(JSP(obj, true)) // prints the full object with newlines and indentation
  */
-export function JSP(obj, prettyPrint = 2): string {
+export function JSP(obj: any, space: string | number = 2): string {
   const propNames = getAllPropertyNames(obj)
   const fullObj = propNames.reduce((acc, propName) => {
     acc[propName] = obj[propName]
     return acc
   }, {})
-  return JSON.stringify(fullObj, null, prettyPrint ?? null)
+  return JSON.stringify(fullObj, null, space ?? null)
+}
+
+/**
+ * Console.logs all property names/values of an object to console with text preamble
+ * @author @dwertheimer
+ *
+ * @param {object} obj
+ * @param {string} preamble - (optional) text to prepend to the output
+ * @param {string | number} space - A String or Number of spaces that's used to insert white space (including indentation, line break characters, etc.) into the output JSON string for readability purposes.
+ * @example clo(obj, 'myObj:')
+ */
+export function clo(obj: any, preamble: string = '', space: string | number = 2): void {
+  console.log(`${preamble !== '' ? `${preamble} ` : ''}${JSP(obj, space)}`)
 }
 
 /**
  * Create a list of the properties of an object, including inherited properties (which are not typically visible in JSON.stringify)
  * @author @dwertheimer
- * 
- * @param {object} obj
+ *
+ * @param {object} inObj
  * @returns [string]
  * @reference https://stackoverflow.com/questions/13796360/javascript-get-all-properties-of-an-object
  */
-export function getAllPropertyNames(obj): string {
+export function getAllPropertyNames(inObj: any): Array<string> {
   const p = []
-  for (; obj != null; obj = Object.getPrototypeOf(obj)) {
+  for (let obj = { ...inObj }; obj != null; obj = Object.getPrototypeOf(obj)) {
     const op = Object.getOwnPropertyNames(obj)
-    for (let i = 0; i < op.length; i++)
+    for (let i = 0; i < op.length; i++) {
       if (/^__/.test(op[i]) === false && op[i] !== 'constructor' && p.indexOf(op[i]) === -1) p.push(op[i])
+    }
   }
   return p
 }
 
 /**
- * Print to the console log, the properties of an object (including its prototype/private methods). This works and is good if you want to know which properties are on the object vs the prototype because it will display in two lines, but it's more succinct to use getAllPropertyNames()
+ * Print to the console log, the properties of an object (including its prototype/private methods). This is useful if you want to know which properties are on the object vs the prototype because it will display in two lines, but it's more succinct to use getAllPropertyNames()
  * @author @dwertheimer
- * 
+ *
  * @param {object} obj
  * @returns {void}
  */
 // This works and is good if you want to know which properties are on the object vs the prototype
 // because it will display in two lines
-export function logAllPropertyNames(obj): void {
+export function logAllPropertyNames(obj: any): void {
   if (obj == null) return // recursive approach
   console.log(Object.getOwnPropertyNames(obj).filter((x) => /^__/.test(x) === false))
   logAllPropertyNames(Object.getPrototypeOf(obj))
@@ -53,18 +69,9 @@ export function logAllPropertyNames(obj): void {
 
 /**
  * Print to the console log all contents of the environment function, introduced in v3.3.2
- * @author @jgclark
+ *
+ * @author @jgclark/@dwertheimer
  */
-export function logAllEnvironmentSettings(): void {
-  console.log(`Contents of environment()...:`)
-  console.log("- .languageCode: " + environment().languageCode)
-  console.log("- .preferredLanguages: " + environment().preferredLanguages)
-  console.log("- .regionCode: " + environment().regionCode)
-  console.log("- .is12hFormat: " + environment().is12hFormat)
-  console.log("- .secondsFromGMT: " + environment().secondsFromGMT)
-  console.log("- .localTimeZoneAbbreviation: " + environment().localTimeZoneAbbreviation)
-  console.log("- .localTimeZoneIdentifier: " + environment().localTimeZoneIdentifier)
-  console.log("- .isDaylightSavingTime: " + environment().isDaylightSavingTime)
-  console.log("- .daylightSavingTimeOffset: " + environment().daylightSavingTimeOffset)
-  console.log("- .nextDaylightSavingTimeTransition: " + environment().nextDaylightSavingTimeTransition)
+export function logEnvironmentSettings(envObj: any): void {
+  clo(envObj)
 }
