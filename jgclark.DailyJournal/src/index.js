b93624ccc594205bc14dc6b00c1cdf602c7771eb
Eduard Metzger <eduard.metzger@googlemail.com>
2022-02-03 15:10:56 -0500

    Add more logging for QuickCapture and DailyJournal around the migration code

--- jgclark.DailyJournal/src/index.js
+++ jgclark.DailyJournal/src/index.js
@@ -17,10 +17,16 @@ const PLUGIN_ID = 'dailyJournal'
 
 export async function onUpdateOrInstall(config: any = { silent: false }): Promise<void> {
   try {
-    console.log(`onUpdateOrInstall running for ${PLUGIN_ID}`)
+    console.log(`${PLUGIN_ID}: onUpdateOrInstall running`)
     // migrate _configuration data to data/<plugin>/settings.json (only executes migration once)
     const migrationResult: number = await migrateConfiguration(PLUGIN_ID, pluginJson, config?.silent)
+    console.log(`${PLUGIN_ID}: onUpdateOrInstall migrateConfiguration code: ${migrationResult}`)
+    if (migrationResult === 0) {
+       const updateSettings = updateSettingData(pluginJson)
+       console.log(`${PLUGIN_ID}: onUpdateOrInstall updateSettingData code: ${updateSettings}`)
+     }
   } catch (error) {
     console.log(error)
   }
+  console.log(`${PLUGIN_ID}: onUpdateOrInstall finished`)
 }
