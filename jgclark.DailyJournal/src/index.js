eb6b9fa909abc87df8d1b1251ccf08e5226b2a94
Eduard Metzger <eduard.metzger@googlemail.com>
2022-02-03 14:23:18 -0500

    Add config migration to Daily Journal

--- jgclark.DailyJournal/src/index.js
+++ jgclark.DailyJournal/src/index.js
@@ -6,4 +6,21 @@
 // v0.6.5, 28.6.2021
 //-----------------------------------------------------------------------------
 
+import pluginJson from '../plugin.json'
+import { migrateConfiguration } from '../../helpers/configuration'
+
+
 export { dayStart, dayReview, todayStart } from './journal'
+
+
+const PLUGIN_ID = 'dailyJournal'
+
+export async function onUpdateOrInstall(config: any = { silent: false }): Promise<void> {
+  try {
+    console.log(`onUpdateOrInstall running for ${PLUGIN_ID}`)
+    // migrate _configuration data to data/<plugin>/settings.json (only executes migration once)
+    const migrationResult: number = await migrateConfiguration(PLUGIN_ID, pluginJson, config?.silent)
+  } catch (error) {
+    console.log(error)
+  }
+}
