c1f198cc3f626f0f8139bb0f6b48c380705a50ba
Mike Erickson <codedungeon@gmail.com>
2022-02-04 06:54:03 -0800

    refactor to use NPConfiguration

--- jgclark.EventHelpers/src/index.js
+++ jgclark.EventHelpers/src/index.js
@@ -7,21 +7,16 @@
 //-----------------------------------------------------------------------------
 
 export { timeBlocksToCalendar } from './timeblocks'
-export {
-  listDaysEvents,
-  insertDaysEvents,
-  listMatchingDaysEvents,
-  insertMatchingDaysEvents,
-} from './eventsToNotes'
+export { listDaysEvents, insertDaysEvents, listMatchingDaysEvents, insertMatchingDaysEvents } from './eventsToNotes'
 export { processDateOffsets } from './offsets'
 
 // allow changes in plugin.json to trigger recompilation
-import pluginJson from '../plugin.json' 
+import pluginJson from '../plugin.json'
 
 // Moving to ConfigV2
-import { migrateConfiguration, updateSettingData } from '../../helpers/configuration'
+import { migrateConfiguration, updateSettingData } from '../../helpers/NPConfiguration'
 
-const PLUGIN_ID = "events"
+const PLUGIN_ID = 'events'
 
 // refactor previous variables to new types
 export async function onUpdateOrInstall(config: any = { silent: false }): Promise<void> {
@@ -31,9 +26,9 @@ export async function onUpdateOrInstall(config: any = { silent: false }): Promis
     const migrationResult: number = await migrateConfiguration(PLUGIN_ID, pluginJson, config?.silent)
     console.log(`${PLUGIN_ID}: onUpdateOrInstall migrateConfiguration code: ${migrationResult}`)
     if (migrationResult === 0) {
-       const updateSettings = updateSettingData(pluginJson)
-       console.log(`${PLUGIN_ID}: onUpdateOrInstall updateSettingData code: ${updateSettings}`)
-     }
+      const updateSettings = updateSettingData(pluginJson)
+      console.log(`${PLUGIN_ID}: onUpdateOrInstall updateSettingData code: ${updateSettings}`)
+    }
   } catch (error) {
     console.log(error)
   }
