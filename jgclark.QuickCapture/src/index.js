18563fdc409f8e5697ef49e537fac181b5b86750
Jonathan Clark <jonathan@clarksonline.me.uk>
2022-02-04 00:08:17 +0000

    Change PLUGIN_ID to configKey = 'inbox'

--- jgclark.QuickCapture/src/index.js
+++ jgclark.QuickCapture/src/index.js
@@ -23,21 +23,21 @@ import pluginJson from '../plugin.json'
 // Moving to ConfigV2
 import { migrateConfiguration, updateSettingData } from '../../helpers/configuration'
 
-const PLUGIN_ID = 'QuickCapture'
+const configKey = 'inbox'
 
 // refactor previous variables to new types
 export async function onUpdateOrInstall(config: any = { silent: false }): Promise<void> {
   try {
-    console.log(`${PLUGIN_ID}: onUpdateOrInstall running`)
+    console.log(`${configKey}: onUpdateOrInstall running`)
     // migrate _configuration data to data/<plugin>/settings.json (only executes migration once)
-    const migrationResult: number = await migrateConfiguration(PLUGIN_ID, pluginJson, config?.silent)
-    console.log(`${PLUGIN_ID}: onUpdateOrInstall migrateConfiguration code: ${migrationResult}`)
+    const migrationResult: number = await migrateConfiguration(configKey, pluginJson, config?.silent)
+    console.log(`${configKey}: onUpdateOrInstall migrateConfiguration code: ${migrationResult}`)
     if (migrationResult === 0) {
        const updateSettings = updateSettingData(pluginJson)
-       console.log(`${PLUGIN_ID}: onUpdateOrInstall updateSettingData code: ${updateSettings}`)
+       console.log(`${configKey}: onUpdateOrInstall updateSettingData code: ${updateSettings}`)
      }
   } catch (error) {
     console.log(error)
   }
-  console.log(`${PLUGIN_ID}: onUpdateOrInstall finished`)
+  console.log(`${configKey}: onUpdateOrInstall finished`)
 }
