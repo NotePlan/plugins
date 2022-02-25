eb0fd221ac24c48a67f328eb203a9c93b4fe906e
jgclark <jonathan@clarksonline.me.uk>
2022-02-25 22:23:47 +0000

    Event Helpers v0.11.5

--- jgclark.EventHelpers/src/index.js
+++ jgclark.EventHelpers/src/index.js
@@ -3,9 +3,10 @@
 //-----------------------------------------------------------------------------
 // Event Helpers
 // Jonathan Clark
-// last updated 4.2.2022, for v0.11.0
+// last updated 20.2.2022, for v0.11.5
 //-----------------------------------------------------------------------------
 
+import { log, logWarn, logError } from "@helpers/dev"
 export { timeBlocksToCalendar } from './timeblocks'
 export {
   listDaysEvents,
@@ -19,23 +20,23 @@ export { processDateOffsets } from './offsets'
 import pluginJson from '../plugin.json' 
 
 // Moving to ConfigV2
-import { migrateConfiguration, updateSettingData } from '../../helpers/NPconfiguration'
+import { migrateConfiguration, updateSettingData } from '@helpers/NPconfiguration'
 
 const configKey = "events"
 
 // refactor previous variables to new types
 export async function onUpdateOrInstall(config: any = { silent: false }): Promise<void> {
   try {
-    console.log(`${configKey}: onUpdateOrInstall running`)
+    log(pluginJson, `${configKey}: onUpdateOrInstall running`)
     // migrate _configuration data to data/<plugin>/settings.json (only executes migration once)
     const migrationResult: number = await migrateConfiguration(configKey, pluginJson, config?.silent)
-    console.log(`${configKey}: onUpdateOrInstall migrateConfiguration code: ${migrationResult}`)
+    log(pluginJson, `${configKey}: onUpdateOrInstall migrateConfiguration code: ${migrationResult}`)
     if (migrationResult === 0) {
        const updateSettings = updateSettingData(pluginJson)
-       console.log(`${configKey}: onUpdateOrInstall updateSettingData code: ${updateSettings}`)
+       log(pluginJson, `${configKey}: onUpdateOrInstall updateSettingData code: ${updateSettings}`)
      }
   } catch (error) {
-    console.log(error)
+    log(pluginJson, error)
   }
-  console.log(`${configKey}: onUpdateOrInstall finished`)
+  log(pluginJson, `${configKey}: onUpdateOrInstall finished`)
 }
