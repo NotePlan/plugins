2fda70b42210a9cdc17a6b2844bb64e62b97b60d
jgclark <jonathan@clarksonline.me.uk>
2022-03-02 17:21:07 +0000

    Filer v0.6.1 fix removals

--- jgclark.Filer/src/fileItems.js
+++ jgclark.Filer/src/fileItems.js
@@ -2,12 +2,14 @@
 // ----------------------------------------------------------------------------
 // Plugin to help move selected selectedParagraphs to other notes
 // Jonathan Clark
-// last updated 12.2.2022 for v0.6.0
+// last updated 2.3.2022 for v0.6.1
 // ----------------------------------------------------------------------------
 
+import pluginJson from "../plugin.json"
+import type { EventsConfig } from '../../helpers/NPCalendar'
 import { castBooleanFromMixed, castStringFromMixed, } from '../../helpers/dataManipulation'
 import { todaysDateISOString, } from '../../helpers/dateTime'
-import { clo } from '../../helpers/dev'
+import { clo, log, logError, logWarn } from '../../helpers/dev'
 import { displayTitle } from '../../helpers/general'
 import { allNotesSortedByChanged } from '../../helpers/note'
 import {
@@ -19,7 +21,6 @@ import {
 } from '../../helpers/paragraph'
 import { chooseHeading } from '../../helpers/userInput'
 import { getOrMakeConfigurationSection } from '../../nmn.Templates/src/configuration'
-import type { EventsConfig } from '../../helpers/NPCalendar'
 
 //-----------------------------------------------------------------------------
 // // Get settings
@@ -35,15 +36,16 @@ type FilerConfig = {
 }
 
 async function getFilerSettings(): Promise<FilerConfig> {
-  console.log(`Start of getFilerSettings()`)
+  log(pluginJson, `Start of getFilerSettings()`)
   let config: FilerConfig
 
   const v2Config: FilerConfig = DataStore.settings
   // $FlowFixMe[incompatible-call]
-  // clo(v2Config, 'v2Config')
+  // clo(v2Config, 'v2Config settings:')
 
   if (v2Config != null && Object.keys(v2Config).length > 0) {
     config = v2Config
+    log(pluginJson, `Using V2 config`)
   } else {
     // Get config settings from Template folder _configuration note or ConfigV2
     const v1config = await getOrMakeConfigurationSection(
@@ -54,7 +56,7 @@ async function getFilerSettings(): Promise<FilerConfig> {
 
     
     if (v1config == null || Object.keys(v1config).length === 0) {
-      console.log(`\tInfo: couldn't find '${configKey}' settings. Will use defaults.`)
+      log(pluginJson, `Info: couldn't find '${configKey}' settings. Will use defaults.`)
       config = {
         addDateBacklink: false,
         dateRefStyle: 'link',
@@ -63,7 +65,7 @@ async function getFilerSettings(): Promise<FilerConfig> {
         version: '(no config found)'
       }
     } else {
-      console.log(`\tFound '${configKey}' settings`)
+      log(pluginJson, `Found '${configKey}' settings`)
       config = {
         addDateBacklink: castBooleanFromMixed(v1config, 'addDateBacklink'),
         dateRefStyle: castStringFromMixed(v1config, 'dateRefStyle'),
@@ -72,9 +74,9 @@ async function getFilerSettings(): Promise<FilerConfig> {
         version: '(v1Config)'
       }
     }
+    // $FlowFixMe
+    // clo(config, `\t${configKey} V1 _config settings:`)
   }
-  // $FlowFixMe
-  clo(config, `\t${configKey} settings from V1 _config:`)
   return config
 }
 
@@ -99,7 +101,7 @@ export async function moveParas(): Promise<void> {
   const { content, paragraphs, selectedParagraphs, note } = Editor
   if (content == null || selectedParagraphs == null || note == null) {
     // No note open, or no selectedParagraph selection (perhaps empty note), so don't do anything.
-    console.log('warning: No note open, so stopping.')
+    logWarn(pluginJson, 'No note open, so stopping.')
     return
   }
 
@@ -111,12 +113,17 @@ export async function moveParas(): Promise<void> {
   // TODO: First check progress.js for getSelectedParaIndex() which does some of this.
   const selection = Editor.selection
   if (selection == null) {
-    console.log('warning: No selection found, so stopping.')
+    logWarn(pluginJson, 'No selection found, so stopping.')
     return
   }
-  const firstSelParaIndex = selectedLinesIndex(selection, paragraphs)
-  const parasInBlock: Array<TParagraph> =
-    getParagraphBlock(note, firstSelParaIndex, config.useExtendedBlockDefinition)
+  // Get paragraph indexes for the start and end of the selection (can be the same)
+  // const firstSelParaIndex = selectedLinesIndex(selection, paragraphs)
+  const [firstSelParaIndex, lastSelParaIndex] = selectedLinesIndex(selection, paragraphs)
+
+  // Get paragraphs for the selection or block
+  const parasInBlock: Array<TParagraph> = (lastSelParaIndex != firstSelParaIndex)
+      ? selectedParagraphs
+      : getParagraphBlock(note, firstSelParaIndex, config.useExtendedBlockDefinition)
 
   // If this is a calendar note we've moving from, and the user wants to
   // create a date backlink, then append backlink to the first selectedPara in parasInBlock
@@ -139,12 +146,12 @@ export async function moveParas(): Promise<void> {
   )
   const destNote = notes[res.index]
   // TODO(EduardMe): The problem is that the showOptions returns the first item if something else is typed. And I can't see a way to distinguish between the two.
-  console.log(displayTitle(destNote)) // NB: -> first item in list (if a new item is typed)
+  // log(pluginJson, displayTitle(destNote)) // NB: -> first item in list (if a new item is typed)
 
   // Ask to which heading to add the selectedParas
   const headingToFind = (await chooseHeading(destNote, true)).toString() // don't know why this coercion is required for flow
 
-  console.log(`  Moving to note: ${destNote.title ?? 'untitled'} under heading: '${headingToFind}'`)
+  log(pluginJson, `  Moving to note: ${destNote.title ?? 'untitled'} under heading: '${headingToFind}'`)
 
   // Add to new location
   // Currently there's no API function to deal with multiple selectedParagraphs,
@@ -154,10 +161,10 @@ export async function moveParas(): Promise<void> {
   let insertionIndex = null
   if (headingToFind === destNote.title || headingToFind.includes('(top of note)')) { // i.e. the first line in project or calendar note
     insertionIndex = calcSmartPrependPoint(destNote)
-    console.log(`  -> top of note, line ${insertionIndex}`)
+    log(pluginJson, `  -> top of note, line ${insertionIndex}`)
   } else if (headingToFind.includes('(bottom of note)')) {
     insertionIndex = destNoteParas.length + 1
-    console.log(`  -> bottom of note, line ${insertionIndex}`)
+    log(pluginJson, `  -> bottom of note, line ${insertionIndex}`)
   } else {
     for (let i = 0; i < destNoteParas.length; i++) {
       const p = destNoteParas[i]
@@ -166,17 +173,17 @@ export async function moveParas(): Promise<void> {
         break
       }
     }
-    console.log(`  -> other heading, line ${String(insertionIndex)}`)
+    log(pluginJson, `  -> other heading, line ${String(insertionIndex)}`)
   }
   if (insertionIndex === null) {
-    console.log(`  Error: insertionIndex is null. Stopping.`)
+    logError(pluginJson, `insertionIndex is null. Stopping.`)
     return
   }
-  // console.log(`  Inserting at index ${insertionIndex}`)
+  log(pluginJson, `Inserting ${parasInBlock.length} paras at index ${insertionIndex} into new note`)
   await destNote.insertParagraph(selectedParasAsText, insertionIndex, 'empty')
 
   // delete from existing location
-  // console.log(`  About to remove ${parasInBlock.length} selectedParas (parasInBlock)`)
+  log(pluginJson, `Removing ${parasInBlock.length} paras from original note`)
   note.removeParagraphs(parasInBlock)
 }
 
@@ -184,7 +191,7 @@ export async function moveParas(): Promise<void> {
 // export function testStart(): void {
 //   if (Editor.note != null) {
 //     const result: number = findStartOfActivePartOfNote(Editor.note)
-//     console.log(result)
+//     log(pluginJson, result)
 //   }
 // }
 
@@ -216,9 +223,7 @@ export function getParagraphBlock(
   const allParas = note.paragraphs
   let startLine = selectedParaIndex
   let selectedPara = allParas[startLine]
-  console.log(
-    `  getParaBlock: ${selectedParaIndex} ${selectedPara.type} '${selectedPara.content}'`,
-  )
+  log(pluginJson, `  getParaBlock: ${selectedParaIndex} ${selectedPara.type} '${selectedPara.content}'`)
 
   if (useExtendedBlockDefinition) {
     // include line unless we hit a new heading, an empty line, or a less-indented line
@@ -226,24 +231,24 @@ export function getParagraphBlock(
     // First look earlier to find earlier lines up to a blank line or horizontal rule
     for (let i = selectedParaIndex - 1; i > startOfActiveSection; i--) {
       const p = allParas[i]
-      // console.log(`  ${i} / indent ${p.indents} / ${p.content}`)
+      // log(pluginJson, `  ${i} / indent ${p.indents} / ${p.content}`)
       if (p.type === 'separator') {
-        console.log(`Found separator line`)
+        log(pluginJson, `Found separator line`)
         startLine = i + 1
         break
       } else if (p.content === '') {
-        console.log(`Found blank line`)
+        log(pluginJson, `Found blank line`)
         startLine = i + 1
         break
       } else if (p.type === 'title') {
-        console.log(`Found heading`)
+        log(pluginJson, `Found heading`)
         startLine = i
         // parasInBlock.unshift(p) // save para onto front, but then stop
         break
       }
       // parasInBlock.unshift(p) // save para onto front
     }
-    console.log(`For extended block worked back and will now start at line ${startLine}`)
+    log(pluginJson, `For extended block worked back and will now start at line ${startLine}`)
   }
 
   selectedPara = allParas[startLine]
@@ -252,54 +257,54 @@ export function getParagraphBlock(
   if (selectedPara.type === 'title') {
     // includes all heading levels
     const thisHeadingLevel = selectedPara.headingLevel
-    console.log(`  Found heading level ${thisHeadingLevel}`)
+    log(pluginJson, `  Found heading level ${thisHeadingLevel}`)
     parasInBlock.push(selectedPara) // make this the first line to move
     // Work out how far this section extends. (NB: headingRange doesn't help us here.)
     for (let i = startLine + 1; i < endOfActiveSection; i++) {
       const p = allParas[i]
       if (p.type === 'title' && p.headingLevel <= thisHeadingLevel) {
-        // console.log(`Found new heading of same or higher level`)
+        // log(pluginJson, `Found new heading of same or higher level`)
         break
       } 
       if (p.type === 'separator') {
-        // console.log(`Found HR`)
+        // log(pluginJson, `Found HR`)
         break
       } else if (p.content === '') {
-        // console.log(`Found blank line`)
+        // log(pluginJson, `Found blank line`)
         break
       }
       parasInBlock.push(p)
     }
-    // console.log(`  Found ${parasInBlock.length} heading section lines`)
+    // log(pluginJson, `  Found ${parasInBlock.length} heading section lines`)
   } else {
     // This isn't a heading.
     const startingIndentLevel = selectedPara.indents
-    console.log(`  Found single line with indent level ${startingIndentLevel}`)
+    log(pluginJson, `  Found single line with indent level ${startingIndentLevel}`)
     parasInBlock.push(selectedPara)
 
     // See if there are following indented lines to move as well
     for (let i = startLine + 1; i < endOfActiveSection; i++) {
       const p = allParas[i]
-      // console.log(`  ${i} / indent ${p.indents} / ${p.content}`)
+      // log(pluginJson, `  ${i} / indent ${p.indents} / ${p.content}`)
       // stop if horizontal line
       if (p.type === 'separator') {
-        // console.log(`Found HR`)
+        // log(pluginJson, `Found HR`)
         break
       } else if (p.content === '') {
-        // console.log(`Found blank line`)
+        // log(pluginJson, `Found blank line`)
         break
       } else if (p.indents <= startingIndentLevel) {
         // stop as this selectedPara is same or less indented than the starting line
-        // console.log(`Stopping as found lower indent`)
+        // log(pluginJson, `Stopping as found lower indent`)
         break
       }
       parasInBlock.push(p) // add onto end of array
     }
   }
 
-  console.log(`  Found ${parasInBlock.length} paras in block:`)
+  log(pluginJson, `  Found ${parasInBlock.length} paras in block:`)
   for (const pib of parasInBlock) {
-    console.log(`    ${pib.content}`)
+    log(pluginJson, `    ${pib.content}`)
   }
   return parasInBlock
 }
