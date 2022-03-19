3b7a82d9874e3f6710851e2602d1f17b9e38e584
David Wertheimer <dwertheimer@users.noreply.github.com>
2022-03-18 18:33:08 -0700

    Forgot to push this file

--- dwertheimer.TaskAutomations/src/markTasks.js
+++ dwertheimer.TaskAutomations/src/markTasks.js
@@ -8,7 +8,7 @@ async function setTasks(dir) {
   console.log(`setTasks: ${String(paragraphs.length || 'zero')} paragraphs`)
   console.log(`setTasks; setting to: ${dir || 'null'}`)
   let find, setVal
-  if (dir === 'open') {
+  if (dir === 'open' || dir === 'openToday') {
     find = 'done'
     setVal = 'open'
   } else {
@@ -17,20 +17,19 @@ async function setTasks(dir) {
     setVal = 'done'
   }
   paragraphs.forEach((para, i) => {
-    console.log(
-      `${i}: ${para.type} ${para.content} ${
-        para.type === find ? `>> SETTING TO: ${setVal}` : ''
-      }`,
-    )
-    if (para.type === find) para.type = setVal
+    console.log(`${i}: ${para.type} ${para.content} ${para.type === find ? `>> SETTING TO: ${setVal}` : ''}`)
+    if (para.type === find) {
+      para.type = setVal
+    }
+    if (dir === 'openToday' && para.type === 'open') {
+      para.content = para.content.replace(/ *>today/gm, '').replace(/ *\@done\(.*\)/gm, '')
+      para.content = para.content + ' >today'
+    }
     Editor.updateParagraph(para)
   })
 }
 
-export default async function markTasks(
-  mark: Direction,
-  withConfirmation: boolean = true,
-) {
+export default async function markTasks(mark: Direction, withConfirmation: boolean = true) {
   console.log(`Starting markTasks(markDone=${mark || 'null'})`)
   //   modifyExistingParagraphs()
   //   return
@@ -40,6 +39,7 @@ export default async function markTasks(
       `Mark all tasks in note as:`,
       [
         { label: 'Open', value: 'open' },
+        { label: 'Open + tagged ">today"', value: 'openToday' },
         { label: 'Completed', value: 'done' },
         { label: 'Cancel', value: null },
       ],
@@ -47,15 +47,14 @@ export default async function markTasks(
     )
   }
   if (dir === 'Cancel') {
-    console.log(`User chose Cancel`)
+    // console.log(`User chose Cancel`)
     return
   } else {
-    const message = `Confirm: Mark ALL ${
-      dir === 'open' ? 'Completed' : 'Open'
-    } tasks as ${dir === 'open' ? 'Open' : 'Completed'}?`
+    const isOpenType = dir === 'open' || dir === 'openToday'
+    const message = `Confirm: Mark ALL ${isOpenType ? 'Completed' : 'Open'} tasks as ${isOpenType ? (dir === 'openToday' ? 'Open+>today' : 'Open') : 'Completed'}?`
     if (withConfirmation) {
       const res = await showMessageYesNo(message)
-      console.log(`User said: ${res}`)
+      // console.log(`User said: ${res}`)
       if (res === 'No') return
     }
   }
