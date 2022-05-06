b2d9004dcc61764557906a9a2675ff0af32f8667
Mike Erickson <codedungeon@gmail.com>
2022-05-06 08:56:41 -0700

    added test stub to np.Globals to satisfy jest when running tests across entire repository

--- np.Globals/__tests__/globals.test.js
+++ np.Globals/__tests__/globals.test.js
@@ -2,4 +2,9 @@
 
 import pluginJson from '../plugin.json'
 
-describe(`${pluginJson['plugin.id']}`, () => {})
+describe(`${pluginJson['plugin.id']}`, () => {
+  it('should pass', () => {
+    // this is stub test to satisfy when running tests across whole repository
+    expect(true).toBe(true)
+  })
+})
