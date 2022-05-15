6de3b8a162a0c430b4732ba94b2e71933c58fb23
David Wertheimer <dwertheimer@users.noreply.github.com>
2022-05-15 06:57:13 -0700

    Fix tagging bug introduced on refactor

--- dwertheimer.TaskAutomations/src/tagTasks.js
+++ dwertheimer.TaskAutomations/src/tagTasks.js
@@ -76,7 +76,7 @@ function getParagraphContainingPosition(note: TNote | Editor, position: number):
  * @param {Array<string>} newTags
  * @returns
  */
-function eliminateExistingTags(existingTags, newTags): Array<string> {
+function getUnduplicatedMergedTagArray(existingTags, newTags): Array<string> {
   let revisedTags = []
   if (newTags.length) {
     if (existingTags.length) {
@@ -92,17 +92,20 @@ function eliminateExistingTags(existingTags, newTags): Array<string> {
 
 /**
  * Append specific hashtags and mentions to a paragraph (if they don't already exist)
- * @param {TParagraph} thisParagraph
+ * @param {string} paraText - the original paragraph text
  * @param {TagsList} tagsToCopy in form of {hashtags: [], mentions: []}
  */
-function updateTextWithTags(thisParagraph: TParagraph, tagsToCopy: TagsList): string | null {
-  const existingTags = getTagsFromString(thisParagraph.content)
-  const mentions = eliminateExistingTags(existingTags.mentions, tagsToCopy.mentions)
-  const hashtags = eliminateExistingTags(existingTags.hashtags, tagsToCopy.hashtags)
+function appendTagsToText(paraText: string, tagsToCopy: TagsList): string | null {
+  log(pluginJson, `appendTagsToText: tagsToCopy.mentions=${tagsToCopy.mentions.toString()}`)
+  const existingTags = getTagsFromString(paraText)
+  log(pluginJson, `existingTags: existingTags.mentions=${existingTags.mentions.toString()}`)
+  const mentions = getUnduplicatedMergedTagArray(existingTags.mentions, tagsToCopy.mentions)
+  const hashtags = getUnduplicatedMergedTagArray(existingTags.hashtags, tagsToCopy.hashtags)
+  log(pluginJson, `appendTagsToText: mentions=${mentions.toString()}`)
   if (hashtags.length || mentions.length) {
     const stuff = `${hashtags.join(' ')} ${mentions.join(' ')}`.trim()
     if (stuff.length) {
-      return `${thisParagraph.content ? `${thisParagraph.content} ` : ''} ${stuff}`.replace(/\s{2,}/gm, ' ')
+      return `${paraText ? `${paraText} ` : ''} ${stuff}`.replace(/\s{2,}/gm, ' ')
     }
   } else {
     console.log('no tags found or no tags need to be copied in list: ', tagsToCopy.toString())
@@ -155,7 +158,7 @@ function copyLineForTags(type: 'hashtags' | 'mentions'): void {
       if (i > 0) {
         // const tags =
         tagsInQuestion.push(tagsInQuestion.shift())
-        const updatedText = updateTextWithTags({ content: contentWithoutTheseTags }, { ...existingTags, ...{ [type]: tagsInQuestion } })
+        const updatedText = appendTagsToText(contentWithoutTheseTags, { ...existingTags, ...{ [type]: tagsInQuestion } })
         if (updatedText) {
           Editor.insertParagraphAfterParagraph(updatedText, thisParagraph, thisParagraph.type)
         }
@@ -191,9 +194,13 @@ export function copyTagsFromLineAbove() {
   const topOfNote = noteType === 'Notes' ? 1 : 0
   if (lineIndex > 0) {
     const prevLineTags = getTagsFromString(getParagraphByIndex(Editor, lineIndex - 1).content)
-    const updatedText = updateTextWithTags(thisParagraph, prevLineTags)
+    const updatedText = appendTagsToText(thisParagraph.content, prevLineTags)
+    log(pluginJson, `copyTagsFromLineAbove: updatedText=${updatedText}`)
     if (updatedText) {
+      clo(thisParagraph, `thisParagraph before:`)
+      thisParagraph.content = updatedText
       Editor.updateParagraph(thisParagraph)
+      clo(thisParagraph, `thisParagraph after:`)
     }
   } else {
     showMessage(`Cannot run this command on the first line of the ${noteType}`)
@@ -215,7 +222,7 @@ export function copyTagsFromHeadingAbove() {
       let headingLineTags = getTagsFromString(heading)
       for (let index = headingPara.lineIndex + 1; index <= thisParagraph.lineIndex; index++) {
         const currentPara = getParagraphByIndex(Editor.note, index)
-        const updatedText = updateTextWithTags(currentPara, headingLineTags)
+        const updatedText = appendTagsToText(currentPara, headingLineTags)
         if (updatedText) {
           Editor.updateParagraph(thisParagraph)
         }
