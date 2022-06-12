e3dfb1968d0b67f5ff277e2fc3aab6318254c3a2
Naman Goel <nmn@fb.com>
2022-06-12 19:39:56 -0400

    Added command to count days to a date. For Clay Russel

---
+++ jgclark.NoteHelpers/src/countDays.js
@@ -0,0 +1,60 @@
+// @flow
+
+import { showMessage } from '@helpers/userInput'
+
+export async function countAndAddDays(): Promise<void> {
+  const note = Editor.note
+  if (!note) {
+    await showMessage('No Note Open')
+    return
+  }
+
+  let insertedCharacters = 0
+  let matches = 0
+  for (let para of note.paragraphs) {
+    if (para.type !== 'list') {
+      continue
+    }
+    if (!para.content.trim().match(/\[\[[0-9]{4}\-[0-9]{2}\-[0-9]{2}\]\]:$/)) {
+      continue
+    }
+
+    const match = para.content.trim().match(/\[\[([0-9]{4})\-([0-9]{2})\-([0-9]{2})\]\]:$/)
+    if (!match) {
+      continue
+    }
+
+    const [_, year, month, date] = match
+    const days = daysUntil(parseInt(year, 10), parseInt(month, 10), parseInt(date, 10))
+    const insertAtIndex = para.contentRange?.end
+    if (insertAtIndex == null) {
+      continue
+    }
+    matches++
+    const stringToInsert = days > 0 ? ` **${days}** days to go!` : days < 0 ? ` **${-days}** ago!` : ` **today!**`
+    Editor.insertTextAtCharacterIndex(` **${days}** days to go!`, insertAtIndex + insertedCharacters)
+    insertedCharacters += stringToInsert.length
+  }
+  note.content = note.content
+  await showMessage(`Added ${matches} dates`)
+}
+
+// function to get msSinceEpoch of given date
+function getMsSinceEpoch(year: number, month: number, date: number): number {
+  const dateObj = new Date(year, month - 1, date)
+  return dateObj.getTime()
+}
+
+// function to get the year, month, and date of today
+function getToday(): [number, number, number] {
+  const dateObj = new Date()
+  return [dateObj.getFullYear(), dateObj.getMonth() + 1, dateObj.getDate()]
+}
+
+// function to count days until given date
+function daysUntil(year: number, month: number, date: number): number {
+  const today = getMsSinceEpoch(...getToday())
+  const target = getMsSinceEpoch(year, month, date)
+  const diff = target - today
+  return Math.ceil(diff / (1000 * 60 * 60 * 24))
+}
