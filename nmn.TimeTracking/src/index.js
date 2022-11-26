36cb144f51c5ad2f680c98974a563e596f425252
Naman Goel <nmn@users.noreply.github.com>
2022-11-26 12:42:06 -0500

    Update dependencies and check lodash (#349)

--- nmn.TimeTracking/src/index.js
+++ nmn.TimeTracking/src/index.js
@@ -1,5 +1,6 @@
 // @flow
 
+import { keys } from 'lodash-es'
 import { getCodeBlocks } from '../../helpers/codeBlocks'
 
 type TData = $ReadOnly<{
@@ -11,19 +12,20 @@ type TData = $ReadOnly<{
   >,
 }>
 
+export function objectKey<Obj: { ... }>(object: Obj): Array<$Keys<Obj>> {
+  return keys(object)
+}
+
 function getOrMadeDataFile() {
-  let dataFile = DataStore.projectNotes.find(
-    (n) => n.filename === '_time_tracking/data.md' || n.filename === '_time_tracking/data.txt',
-  )
+  let dataFile = DataStore.projectNotes.find((n) => n.filename === '_time_tracking/data.md' || n.filename === '_time_tracking/data.txt')
   if (dataFile == null) {
     DataStore.newNote('data', '_time_tracking')
-    dataFile = DataStore.projectNotes.find(
-      (n) => n.filename === '_time_tracking/data.md' || n.filename === '_time_tracking/data.txt',
-    )
+    dataFile = DataStore.projectNotes.find((n) => n.filename === '_time_tracking/data.md' || n.filename === '_time_tracking/data.txt')
   }
   return dataFile
 }
 
+// eslint-disable-next-line no-unused-vars
 function getConfig(): ?TData {
   const configFile = getOrMadeDataFile()
   if (configFile == null) {
@@ -38,10 +40,11 @@ function getConfig(): ?TData {
   }
 }
 
+// eslint-disable-next-line no-unused-vars
 function setConfig(data: TData): null | void {
   const configFile = getOrMadeDataFile()
   if (configFile == null) {
     return null
   }
-  configFile.content = `${configFile.paragraphs[0].content  }\n\n\`\`\`json\n${  JSON.stringify(data, null, 2)  }\n\`\`\`\n`
+  configFile.content = `${configFile.paragraphs[0].content}\n\n\`\`\`json\n${JSON.stringify(data, null, 2)}\n\`\`\`\n`
 }
