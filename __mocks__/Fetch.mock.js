683a840f18d6c0de3cd573742814e33fe9f20eb3
David Wertheimer <dwertheimer@users.noreply.github.com>
2022-12-15 14:47:15 -0800

    Adding fetch mock for fast/fake server responses

---
+++ __mocks__/Fetch.mock.js
@@ -0,0 +1,44 @@
+// @flow
+
+export type FetchMockResponse = {
+  match: { url: string /* a string to look for in the URL passed to fetch */, optionsBody?: string /* an (optional) string to look for in the options.body passed to fetch */ },
+  response: string /* the response to return if the match is found */,
+}
+
+const defaultResponse = { match: { url: '', optionsBody: '' }, response: 'Default fetch response string (did not match any mocks)' }
+
+/**
+ * Mock the fetch() function to return a specific response for a given URL and options.body
+ * You pass in text to the match object and if the url or options.body contain that text, the response is returned
+ * Note: match.url is required but match.optionsBody is optional
+ * If no match is found, the defaultResponse text is returned
+ * @param {Array<FetchMockResponse>} mockResponses - Array of mock responses in the form of FetchMockResponse
+ * @example
+import response1 from './testJSONs/response1.json' // a JSON file with a sample server response
+import { FetchMock, type FetchMockResponse } from '@mocks/Fetch.mock'
+const OVERRIDE_FETCH = true // set to true to override the global fetch() function with fake responses passed below
+if (OVERRIDE_FETCH) {
+  const fm = new FetchMock([{ match: { url: 'foo', optionsBody: 'bar' }, response: JSON.stringify(response1) }]) // add one object to array for each mock response
+  fetch = async (url, opts) => fm.fetch(url, opts) //override the global fetch
+}
+ * ...then wherever the code is using fetch, it will use the mock
+ * const result = await fetch('http://foo', { body: 'has the word bar in it' }) // returns 'fake server response here' (the response text)
+ */
+export class FetchMock {
+  responses: Array<FetchMockResponse> = [defaultResponse]
+  constructor(mockResponses: Array<FetchMockResponse>) {
+    if (mockResponses && !Array.isArray(mockResponses)) throw new Error('Fetch constructor requires an array of mock responses')
+    this.responses = [...(mockResponses?.length ? mockResponses : []), ...this.responses]
+  }
+  fetch(url: string, options: FetchOptions) {
+    const body = options?.body ?? null //
+    const match = this.responses.find((r) => {
+      const urlTest = r.match?.url ? new RegExp(r.match.url).test(url) : false
+      // body options will return true if it's a match or if it's not defined
+      const optionsBodyTest = r.match?.optionsBody && options.body ? new RegExp(r.match.optionsBody).test(options.body) : r.match?.optionsBody ? false : true
+      return urlTest && optionsBodyTest
+    })
+    // return match ? Promise.resolve(match.response) : Promise.resolve(defaultResponse.response)
+    return match ? match.response : defaultResponse.response
+  }
+}
---
+++ __mocks__/__tests__/fetch.mock.test.js
@@ -0,0 +1,79 @@
+/* global jest, describe, test, expect, beforeAll */
+import { CustomConsole } from '@jest/console' // see note below
+import { FetchMock } from '../Fetch.mock'
+import { simpleFormatter } from '@mocks/index'
+
+const PLUGIN_NAME = `Fetch.mock`
+const FILENAME = ``
+
+beforeAll(() => {
+  global.console = new CustomConsole(process.stdout, process.stderr, simpleFormatter) // minimize log footprint
+})
+
+/* Samples:
+expect(result).toMatch(/someString/)
+expect(result).not.toMatch(/someString/)
+expect(result).toEqual([])
+import { mockWasCalledWith } from '@mocks/mockHelpers'
+      const spy = jest.spyOn(console, 'log')
+      const result = mainFile.getConfig()
+      expect(mockWasCalledWith(spy, /config was empty/)).toBe(true)
+      spy.mockRestore()
+
+      test('should return the command object', () => {
+        const result = f.getPluginCommands({ 'plugin.commands': [{ a: 'foo' }] })
+        expect(result).toEqual([{ a: 'foo' }])
+      })
+*/
+
+describe(`${PLUGIN_NAME}`, () => {
+  describe(`${FILENAME}`, () => {
+    //functions go here using jfunc command
+    /*
+     * constructor()
+     */
+    describe('constructor()' /* function */, () => {
+      test('should create fetch with default response', () => {
+        const f = new FetchMock()
+        expect(f.responses.length).toEqual(1)
+        expect(f.responses[0].response).toMatch(/did not match/)
+      })
+      test('should create fetch with supplied response', () => {
+        const f = new FetchMock([{ match: { url: 'foo', optionsBody: 'bar' }, response: 'baz' }])
+        expect(f.responses.length).toEqual(2)
+        expect(f.responses[0].response).toMatch(/baz/)
+        expect(f.responses[1].response).toMatch(/did not match/)
+      })
+      test('should return default response with no match', async () => {
+        const f = new FetchMock()
+        const result = await f.fetch()
+        expect(result).toMatch(/did not match/)
+      })
+      test('should return match if both url and body match', async () => {
+        const f = new FetchMock([{ match: { url: 'foo', optionsBody: 'bar' }, response: 'baz' }])
+        const result = await f.fetch('http://foo', { body: 'does it include bar' })
+        expect(result).toMatch(/baz/)
+      })
+      test('should return default if url matches but not body', async () => {
+        const f = new FetchMock([{ match: { url: 'foo', optionsBody: 'bar' }, response: 'baz' }])
+        const result = await f.fetch('http://foo', { body: 'does it include xxx' })
+        expect(result).toMatch(/did not match/)
+      })
+      test('should return default if body matches but not url', async () => {
+        const f = new FetchMock([{ match: { url: 'fun', optionsBody: 'bar' }, response: 'baz' }])
+        const result = await f.fetch('http://foo', { body: 'does it include bar' })
+        expect(result).toMatch(/did not match/)
+      })
+      test('should return match if url matches and body is blank', async () => {
+        const f = new FetchMock([{ match: { url: 'foo', optionsBody: '' }, response: 'baz' }])
+        const result = await f.fetch('http://foo', { body: 'does it include bar' })
+        expect(result).toMatch(/baz/)
+      })
+      test('should return match if url matches and body is not passed', async () => {
+        const f = new FetchMock([{ match: { url: 'foo' }, response: 'baz' }])
+        const result = await f.fetch('http://foo', { body: 'does it include bar' })
+        expect(result).toMatch(/baz/)
+      })
+    })
+  })
+})
