96a6ec807fa97e1ac65a4ddd18bc53dc12f5a66a
jgclark <jonathan@clarksonline.me.uk>
2022-12-15 18:39:42 +0000

    Add .isHidden boolean to plugin object and command

--- flow-typed/Noteplan.js
+++ flow-typed/Noteplan.js
@@ -697,6 +697,10 @@ type PluginCommandObject = {
    */
   +pluginName: string,
   /**
+   * Whether this is marked as a hidden command (getter)
+   */
+  +isHidden: boolean,
+  /**
    * List of optional argument descriptions for the specific command (getter). Use this if you want to invoke this command from another plugin to inform the user what he nees to enter for example.
    */
   +arguments: $ReadOnlyArray<string>,
@@ -738,6 +742,10 @@ type PluginObject = {
    * This is the online data of the plugin. It might not be installed locally. (getter)
    */
   +isOnline: boolean,
+  /**
+   * Whether this plugin is marked as hidden (getter)
+   */
+  +isHidden: boolean,
   /**
    * Script filename that contains the code for this plugin (like script.js) (getter)
    */
