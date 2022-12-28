22810a6773266f55f4b60d47a3edef451b984828
Aaron Gascoigne <atlrudeboi1@gmail.com>
2022-12-28 16:43:19 -0500

    Review comments
    Signed-off-by: Aaron Gascoigne <atlrudeboi1@gmail.com>

--- aaronpoweruser.ReadwiseUnofficial/src/NPReadwise.js
+++ aaronpoweruser.ReadwiseUnofficial/src/NPReadwise.js
@@ -1,5 +1,6 @@
 // @flow
 import { insertContentUnderHeading } from "../../helpers/NPParagraph"
+import { showMessage } from "../../helpers/userInput";
 import pluginJson from '../plugin.json'
 import { log, logDebug, logError, logWarn, clo, JSP } from '@helpers/dev'
 import { getOrMakeNote } from '@helpers/note'
@@ -9,6 +10,14 @@ export async function readwiseSync(): Promise<void> {
   const settings = DataStore.settings
   const accessToken = settings.accessToken ?? ''
   logDebug(pluginJson, `access token is : ${accessToken}`)
+
+  if (accessToken === '') {
+    showMessage(pluginJson, 'No access token found. Please add your Readwise access token in the plugin settings.')
+    return
+  } else if (accessToken.length !== 50) {
+    showMessage(pluginJson, 'Invalid access token. Please check your Readwise access token in the plugin settings.')
+    return
+  }
 
   const response = await getReadwise()
   response.map(parseBook)
@@ -41,29 +50,33 @@ async function getReadwise(): Promise<any> {
 }
 
 async function parseBook(source) {
-  const title = source.readable_title
-  const author = source.author
-  const category = source.category
-  const highlights = source.highlights
-  let metadata = `author: [[${author}]]` + '\n' + `Category: [[${category}]]` + '\n'
-  if (source.book_tags !== null) {
-     metadata += `Document tags: ${source.book_tags.map(tag => `#${tag.name} `).join(', ')}\n`
+  try {
+    const title = source.readable_title
+    const author = source.author
+    const category = source.category
+    const highlights = source.highlights
+    let metadata = `author: [[${author}]]` + '\n' + `Category: [[${category}]]` + '\n'
+    if (source.book_tags !== null) {
+      metadata += `Document tags: ${source.book_tags.map(tag => `#${tag.name} `).join(', ')}\n`
+    }
+    if (source.unique_url !== null) {
+      metadata += `URL: ${source.unique_url}`
+    }
+    const rootFolder = DataStore.settings.baseFolder ?? 'Readwise'
+    let baseFolder = rootFolder
+    if (DataStore.settings.groupByType === 'true') {
+      baseFolder = `${rootFolder}/${category}`
+    }
+    log(pluginJson, `base folder is : ${baseFolder}`)
+    const outputNote = await getOrMakeNote(title, baseFolder, '')
+    // TODO: This is adding empty newlines to the note need to fix.
+    await insertContentUnderHeading(outputNote, 'Highlights', '', 2)
+    // Probably shouldn't depend on this sorting and order by hand
+    await insertContentUnderHeading(outputNote, 'Metadata', metadata, 2)
+    highlights.map(highlight => appendToNote(highlight, outputNote))
+  } catch (error) {
+      logError(pluginJson, error)
   }
-  if (source.unique_url !== null) {
-    metadata += `URL: ${source.unique_url}`
-  }
-  const rootFolder = DataStore.settings.baseFolder ?? 'Readwise'
-  let baseFolder = rootFolder
-  if (DataStore.settings.groupByType === 'true') {
-    baseFolder = `${rootFolder}/${category}`
-  }
-  log(pluginJson, `base folder is : ${baseFolder}`)
-  const outputNote = await getOrMakeNote(title, baseFolder, '')
-  // TODO: This is adding empty newlines to the note need to fix.
-  await insertContentUnderHeading(outputNote, 'Highlights', '', 2)
-  // Probably shouldn't depend on this sorting and order by hand
-  await insertContentUnderHeading(outputNote, 'Metadata', metadata, 2)
-  highlights.map(highlight => appendToNote(highlight, outputNote))
 }
 
 function appendToNote(highlight, note) {
