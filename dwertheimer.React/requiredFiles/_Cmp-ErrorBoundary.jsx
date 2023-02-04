1c88abc14bf016883c9f79474c10e9458b08a824
David Wertheimer <dwertheimer@users.noreply.github.com>
2023-02-04 12:38:38 -0800

    Removing React WIP from codebase

--- dwertheimer.React/requiredFiles/_Cmp-ErrorBoundary.jsx
+++
@@ -1,9 +0,0 @@
-const ErrorFallback = ({ error, resetErrorBoundary }) => {
-  return (
-    <div role="alert">
-      <h1>Something went wrong in React:</h1>
-      <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
-    </div>
-  )
-}
