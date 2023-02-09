04c064e4b3b2a8d94a680553b37d65f3e3bb5f41
David Wertheimer <dwertheimer@users.noreply.github.com>
2023-02-09 09:38:17 -0800

    Adding bridge functions to talk to React HTML window

--- helpers/HTMLView.js
+++ helpers/HTMLView.js
@@ -5,7 +5,7 @@
 // Last updated 10.10.2022
 // ---------------------------------------------------------
 
-import { clo, logDebug, logError, logWarn } from '@helpers/dev'
+import { clo, logDebug, logError, logWarn, JSP } from '@helpers/dev'
 const pluginJson = 'helpers/HTMLView'
 
 let baseFontSize = 14
@@ -561,7 +561,7 @@ export function getCallbackCodeString(jsFunctionName: string, commandName: strin
     // This is a callback bridge from HTML to the plugin
     const ${jsFunctionName} = (commandName = "${commandName}", pluginID = "${pluginID}", commandArgs = []) => {
       const code = ${haveNotePlanExecute}.replace("%%commandName%%",commandName).replace("%%pluginID%%",pluginID).replace("%%commandArgs%%", JSON.stringify(commandArgs));
-      console.log(\`${jsFunctionName}: Sending command "\$\{commandName\}" to NotePlan: "\$\{pluginID\}" with args: \$\{JSON.stringify(commandArgs)\}\`);
+      // console.log(\`${jsFunctionName}: Sending command "\$\{commandName\}" to NotePlan: "\$\{pluginID\}" with args: \$\{JSON.stringify(commandArgs)\}\`);
       console.log(\`${jsFunctionName}: Sending code: "\$\{code\}"\`)
       if (window.webkit) {
         window.webkit.messageHandlers.jsBridge.postMessage({
@@ -917,4 +917,95 @@ export function rgbToHex(r: number, g: number, b: number): string {
  */
 export function replaceMarkdownLinkWithHTMLLink(str: string): string {
   return str.replace(/\[(.*?)\]\((.*?)\)/gm, `<a href="$2">$1</a>`)
+}
+
+/**
+ * Message action types
+ * SET_TITLE - update the title of the HTML window (send {title: 'new title'} in the payload)
+ * SHOW_BANNER - display a message in the top of the page (use the helper sendBannerMessage('message'))
+ * SET_DATA - tell the HTML window to update its state with the data passed
+ */
+
+/**
+ * Send some data to the HTML window (to be written to globalSharedData) using postMessage message passing
+ * Note: we can (and do) write to globalSharedData directly, but we should try to use this function
+ * to do so, because it will allow us to use message passing to update the state in the HTML window
+ * which gives us more visibility into what's happening on the HTML side
+ * @param {string - see above} actionType - the reducer-type action to be dispatched (tells the app how to act on the data passed)
+ * @param {any} data - the data to be passed to the app (and ultimately to be written to globalSharedData)
+ * @return {any} - the result of the runJavaScript call (should be unimportant in this case -- undefined is ok)
+ * @author @dwertheimer
+ */
+export async function sendToHTMLWindow(actionType: string, data: any = {}): any {
+  try {
+    logDebug(`Bridge::sendToHTMLWindow`, `sending "${actionType}" data=${JSON.stringify(data)}`)
+    const dataWithUpdated = { ...data, ...{ lastUpdated: { msg: actionType, date: new Date().toLocaleString() } } }
+    const result = await HTMLView.runJavaScript(`window.postMessage(
+        { 
+          type: '${actionType}', 
+          payload: ${JSON.stringify(dataWithUpdated)} 
+        }, 
+        '*'
+      );`)
+    // logDebug(`Bridge::sendToHTMLWindow`, `result from the window: ${JSON.stringify(result)}`)
+    return result
+  } catch (error) {
+    logError(pluginJson, JSP(error))
+  }
+}
+
+/**
+ * Get the current state of globalSharedData from the HTML window (SHARED DATA MUST BE OBJECTS)
+ * Returns actual object or undefined if the global var doesn't exist (along with some noisy log errors)
+ * See notes above
+ * NOTE: this function should only be called after the window has fully set up, the global var has been set
+ * @param {string} varName - the name of the global variable to be updated (by default "globalSharedData")
+ * @author @dwertheimer
+ * @returns {Object} - the current state of globalSharedData
+ */
+export async function getGlobalSharedData(varName: string = 'globalSharedData'): any {
+  try {
+    const currentValue = await HTMLView.runJavaScript(`${varName};`)
+    if (currentValue !== undefined) logDebug(`getGlobalSharedData`, `got ${varName}: ${JSON.stringify(currentValue)}`)
+    return currentValue
+  } catch (error) {
+    logError(pluginJson, JSP(error))
+  }
+}
+
+/**
+ * Generally, we will try not to update the global shared object directly, but instead use message passing
+ * to let React update the state. But there will be times we need to update the state
+ * from here (e.g. when we hit limits of message passing)
+ * @param {any} data - the full object to be written to globalSharedData (SHARED DATA MUST BE OBJECTS)
+ * @param {boolean} mergeData - if true (default), will merge the new data with the existing data, if false, will fully overwrite
+ * @param {string} varName - the name of the global variable to be updated (by default "globalSharedData")
+ * @author @dwertheimer
+ * @returns {any} returns the result of the runJavaScript call, which in this case is typically identical to the data passed
+ * ...and so can probably be ignored
+ */
+export async function updateGlobalSharedData(data: any, mergeData: boolean = true, varName: string = 'globalSharedData'): any {
+  let newData
+  const currentData = await getGlobalSharedData(varName)
+  if (currentData === undefined) {
+    logDebug(`updateGlobalSharedData`, `Variable ${varName} was not defined (creating it now)...ignore the WebView error above ^^^`)
+    await HTMLView.runJavaScript(`let ${varName} = {};`) // create the global var if it doesn't exist
+  }
+  if (mergeData) {
+    newData = { ...currentData, ...data }
+  } else {
+    newData = data
+  }
+  logDebug(`updateGlobalSharedData`, `writing globalSharedData (merged=${String(mergeData)}) to ${JSON.stringify(newData)}`)
+  return await HTMLView.runJavaScript(`${varName} = JSON.parse(\`${JSON.stringify(newData)}\`);`)
+}
+
+/**
+ * Send a warning message to the HTML window (displays a warning message at the top of page)
+ * @param {string} message
+ * @param {string} color https://www.w3schools.com/w3css/w3css_colors.asp
+ * @param {string} border (left vertical stripe border of box) https://www.w3schools.com/w3css/w3css_colors.asp
+ */
+export async function sendBannerMessage(message: string, color: string = 'w3-pale-red', border: string = 'w3-border-red'): Promise<any> {
+  return await sendToHTMLWindow('SHOW_BANNER', { warn: true, msg: message, color, border })
 }
