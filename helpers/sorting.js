2a2982ee68fd964f14f175beeacac7ba79c48721
David Wertheimer <dwertheimer@users.noreply.github.com>
2023-02-13 17:09:29 -0800

    added some sorting tests

--- helpers/sorting.js
+++ helpers/sorting.js
@@ -29,14 +29,14 @@ export type GroupedTasks = {
   checklist: Array<SortableParagraphSubset>,
   checklistDone: Array<SortableParagraphSubset>,
   checklistCancelled: Array<SortableParagraphSubset>,
-  checklistScheduled: Array<SortableParagraphSubset>,
+  checklistScheduled: Array<SortableParagraphSubset> /*,
   title: Array<SortableParagraphSubset>,
   quote: Array<SortableParagraphSubset>,
   list: Array<SortableParagraphSubset>,
   empty: Array<SortableParagraphSubset>,
   text: Array<SortableParagraphSubset>,
   code: Array<SortableParagraphSubset>,
-  separator: Array<SortableParagraphSubset>,
+  separator: Array<SortableParagraphSubset>, */,
 }
 
 export type ParagraphsGroupedByType = {
@@ -140,14 +140,12 @@ export const fieldSorter =
         // field = isNaN(field) ? field : Number(field)
         const aFirstValue = firstValue(get(a, field))
         const bFirstValue = firstValue(get(b, field))
-        const aValue = (aFirstValue == null) ? null :
-          isNaN(aFirstValue) ? aFirstValue : Number(aFirstValue)
-        const bValue = (bFirstValue == null) ? null :
-          isNaN(bFirstValue) ? bFirstValue : Number(bFirstValue)
+        const aValue = aFirstValue == null ? null : isNaN(aFirstValue) ? aFirstValue : Number(aFirstValue)
+        const bValue = bFirstValue == null ? null : isNaN(bFirstValue) ? bFirstValue : Number(bFirstValue)
         // if (field === "date") logDebug('', `${field}: ${String(aValue)} (${typeof aValue}) / ${String(bValue)} (${typeof bValue})`)
         if (aValue === bValue) return 0
-        if (aValue == null || aValue === "NaN") return isDesc ? -dir : dir //null or undefined always come last
-        if (bValue == null || bValue === "NaN") return isDesc ? dir : -dir
+        if (aValue == null || aValue === 'NaN') return isDesc ? -dir : dir //null or undefined always come last
+        if (bValue == null || bValue === 'NaN') return isDesc ? dir : -dir
         // $FlowIgnore - flow complains about comparison of non-identical types, but I am trapping for that
         return typeof aValue === typeof bValue ? (aValue > bValue ? dir : -dir) : 0
       })
@@ -156,7 +154,7 @@ export const fieldSorter =
 /**
  * Helper function for fieldSorter fields.
  * Sometimes you want to sort on the value of a field that is an array.
- * If the value is an array, return the first value from it. 
+ * If the value is an array, return the first value from it.
  * If it's not an array, just return the value, and if it's a string, lowercase value.
  * @author @dwertheimer
  * @param {any} val
@@ -168,10 +166,17 @@ export const firstValue = (val: any): string | number => {
     return retVal
   } else {
     retVal = typeof retVal === 'number' || (typeof retVal !== 'object' && !isNaN(retVal) && retVal !== '') ? Number(retVal) : retVal
-    return (typeof retVal === 'string' && retVal !== "NaN") ? retVal.toLowerCase() : retVal
+    return typeof retVal === 'string' && retVal !== 'NaN' ? retVal.toLowerCase() : retVal
   }
 }
 
+/**
+ * A general purpose function to get all the elements from a task that match a regex
+ * and return them as an array. Generally usefull for getting all the tags or mentions from a task
+ * @param {string} content
+ * @param {RegExp} reSearch
+ * @returns {Array<string>} - array of elements found matching the regex
+ */
 export function getElementsFromTask(content: string, reSearch: RegExp): Array<string> {
   const found = []
   let matches = reSearch.exec(content)
@@ -205,8 +210,9 @@ function getNumericPriority(item: SortableParagraphSubset): number {
  * a scheduled task/checklist.)
  * @param {TParagraph} para
  * @returns - the type of the paragraph (the normal types + 'scheduled' and 'checklistScheduled')
+ * @author @dwertheimer
  */
-export function calculateParagraphType(para: TParagraph) {
+export function calculateParagraphType(para: TParagraph): string {
   let type = para.type
   if (type === 'open' && isScheduled(para.content)) type = 'scheduled'
   if (type === 'checklist' && isScheduled(para.content)) type = 'checklistScheduled'
@@ -214,48 +220,58 @@ export function calculateParagraphType(para: TParagraph) {
 }
 
 /**
+ * Take in a paragraph and return a sortable object with all the fields specified in the SortableParagraphSubset type
+ * @param {TParagraph} para
+ * @returns {SortableParagraphSubset} - a sortable object
+ * @author @dwertheimer
+ */
+export function getSortableTask(para: TParagraph): SortableParagraphSubset {
+  const content = para.content
+  const hashtags = getElementsFromTask(content, HASHTAGS)
+  const mentions = getElementsFromTask(content, MENTIONS)
+  const exclamations = getElementsFromTask(content, EXCLAMATIONS)
+  const parensPriority = getElementsFromTask(content, PARENS_PRIORITY)
+  const task: SortableParagraphSubset = {
+    content: para.content,
+    index: para.lineIndex,
+    raw: para.rawContent,
+    hashtags,
+    mentions,
+    exclamations,
+    parensPriority,
+    heading: para.heading,
+    filename: para?.filename || '',
+    indents: para.indents,
+    children: [],
+    due: para.date ?? new Date('2999-12-31'),
+    paragraph: para,
+    calculatedType: calculateParagraphType(para),
+  }
+  // console.log(`new: ${index}: indents:${para.indents} ${para.rawContent}`)
+  task.priority = getNumericPriority(task)
+  return task
+}
+
+/**
  * Sort paragraphs into groups of like types (open, scheduled, done, cancelled, etc.) for task sorting
  * @param {Array<Paragraph>} paragraphs - array of paragraph objects input
  * @param {boolean} ignoreIndents - whether to pay attention to child/indented paragraphs
  * @returns {GroupedTasks} - object of tasks by type {'open':[], 'scheduled'[], 'done':[], 'cancelled':[], etc.}
+ * @author @dwertheimer
  */
 export function getTasksByType(paragraphs: $ReadOnlyArray<TParagraph>, ignoreIndents: boolean = false): GroupedTasks {
-  const tasks = { open: [], scheduled: [], done: [], cancelled: [], title: [], quote: [], list: [], empty: [], text: [], code: [], separator: [] }
-  // * @type {"open", "done", "scheduled", "cancelled", "title", "quote", "list" (= bullet), "empty" (no content) or "text" (= plain text)}
-  TASK_TYPES.forEach((t) => (tasks[t] = []))
+  const tasks = TASK_TYPES.reduce((acc, t) => ({ ...acc, ...{ [t]: [] } }), {})
   let lastParent = { indents: 999, children: [] }
   // clo(paragraphs, 'getTasksByType')
   for (let index = 0; index < paragraphs.length; index++) {
     // console.log(`getTasksByType paragraphs.length:${paragraphs.length}`)
     const para = paragraphs[index]
     // clo(para, 'getTasksByType')
-    // FIXME: non tasks are not going to get through this filter. What to do?
     if (isTask || (!ignoreIndents && para.indents > lastParent.indents)) {
       const content = para.content
       // console.log(`found: ${index}: ${para.type}: ${para.content}`)
       try {
-        const hashtags = getElementsFromTask(content, HASHTAGS)
-        const mentions = getElementsFromTask(content, MENTIONS)
-        const exclamations = getElementsFromTask(content, EXCLAMATIONS)
-        const parensPriority = getElementsFromTask(content, PARENS_PRIORITY)
-        const task: SortableParagraphSubset = {
-          content: para.content,
-          index,
-          raw: para.rawContent,
-          hashtags,
-          mentions,
-          exclamations,
-          parensPriority,
-          heading: para.heading,
-          filename: para?.filename || '',
-          indents: para.indents,
-          children: [],
-          due: para.date ?? new Date('2099-12-31'),
-          paragraph: para,
-          calculatedType: calculateParagraphType(para),
-        }
-        // console.log(`new: ${index}: indents:${para.indents} ${para.rawContent}`)
-        task.priority = getNumericPriority(task)
+        const task = getSortableTask(para)
         if (!ignoreIndents && lastParent.indents < para.indents) {
           lastParent.children.push(task)
         } else {
@@ -271,5 +287,6 @@ export function getTasksByType(paragraphs: $ReadOnlyArray<TParagraph>, ignoreInd
   }
 
   // console.log(`\tgetTasksByType Open Tasks:${tasks.open.length} returning from getTasksByType`)
+  // $FlowFixMe - Flow doesn't like that I am ensuring that all the keys are in the object using reduce above
   return tasks
 }
