7a8b88d4ead576f48f5015e5fecfd1bfeff3456c
David Wertheimer <dwertheimer@users.noreply.github.com>
2023-03-01 22:06:28 -0800

    fix another import

--- scripts/rollup.react.js
+++ scripts/rollup.react.js
@@ -15,7 +15,7 @@ import path from 'path'
 import fg from 'fast-glob' //dbw adding for requiredFiles glob wildcard watch (**/)
 import { babel } from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
-import { terser } from '@rollup/plugin-terser'
+import terser from '@rollup/plugin-terser'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import json from '@rollup/plugin-json'
 import rollup from 'rollup'
