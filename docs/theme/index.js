ba7cd3511ad867a99e098e42bed88e5f3f1e0ce7
David Wertheimer <dwertheimer@users.noreply.github.com>
2023-03-12 00:43:12 -0800

    docs fix

--- docs/theme/index.js
+++ docs/theme/index.js
@@ -5,110 +5,81 @@ var fs = require('fs'),
   _ = require('lodash'),
   concat = require('concat-stream'),
   GithubSlugger = require('github-slugger'),
-  createFormatters = require('../').util.createFormatters,
-  LinkerStack = require('../').util.LinkerStack,
-  hljs = require('highlight.js');
+  createFormatters = require('documentation').util.createFormatters,
+  LinkerStack = require('documentation').util.LinkerStack,
+  hljs = require('highlight.js')
 
 function isFunction(section) {
-  return (
-    section.kind === 'function' ||
-    (section.kind === 'typedef' &&
-      section.type &&
-      section.type.type === 'NameExpression' &&
-      section.type.name === 'Function')
-  );
+  return section.kind === 'function' || (section.kind === 'typedef' && section.type && section.type.type === 'NameExpression' && section.type.name === 'Function')
 }
 
 module.exports = function (comments, config) {
-  var linkerStack = new LinkerStack(config).namespaceResolver(
-    comments,
-    function (namespace) {
-      var slugger = new GithubSlugger();
-      return '#' + slugger.slug(namespace);
-    }
-  );
+  var linkerStack = new LinkerStack(config).namespaceResolver(comments, function (namespace) {
+    var slugger = new GithubSlugger()
+    return '#' + slugger.slug(namespace)
+  })
 
-  var formatters = createFormatters(linkerStack.link);
+  var formatters = createFormatters(linkerStack.link)
 
-  hljs.configure(config.hljs || {});
+  hljs.configure(config.hljs || {})
 
   var sharedImports = {
     imports: {
       slug(str) {
-        var slugger = new GithubSlugger();
-        return slugger.slug(str);
+        var slugger = new GithubSlugger()
+        return slugger.slug(str)
       },
       shortSignature(section) {
-        var prefix = '';
+        var prefix = ''
         if (section.kind === 'class') {
-          prefix = 'new ';
+          prefix = 'new '
         } else if (!isFunction(section)) {
-          return section.name;
+          return section.name
         }
-        return prefix + section.name + formatters.parameters(section, true);
+        return prefix + section.name + formatters.parameters(section, true)
       },
       signature(section) {
-        var returns = '';
-        var prefix = '';
+        var returns = ''
+        var prefix = ''
         if (section.kind === 'class') {
-          prefix = 'new ';
+          prefix = 'new '
         } else if (!isFunction(section)) {
-          return section.name;
+          return section.name
         }
         if (section.returns.length) {
-          returns = ': ' + formatters.type(section.returns[0].type);
+          returns = ': ' + formatters.type(section.returns[0].type)
         }
-        return prefix + section.name + formatters.parameters(section) + returns;
+        return prefix + section.name + formatters.parameters(section) + returns
       },
       md(ast, inline) {
-        if (
-          inline &&
-          ast &&
-          ast.children.length &&
-          ast.children[0].type === 'paragraph'
-        ) {
+        if (inline && ast && ast.children.length && ast.children[0].type === 'paragraph') {
           ast = {
             type: 'root',
-            children: ast.children[0].children.concat(ast.children.slice(1))
-          };
+            children: ast.children[0].children.concat(ast.children.slice(1)),
+          }
         }
-        return formatters.markdown(ast);
+        return formatters.markdown(ast)
       },
       formatType: formatters.type,
       autolink: formatters.autolink,
       highlight(example) {
         if (config.hljs && config.hljs.highlightAuto) {
-          return hljs.highlightAuto(example).value;
+          return hljs.highlightAuto(example).value
         }
-        return hljs.highlight(example, { language: 'js' }).value;
-      }
-    }
-  };
+        return hljs.highlight(example, { language: 'js' }).value
+      },
+    },
+  }
 
-  sharedImports.imports.renderSectionList = _.template(
-    fs.readFileSync(path.join(__dirname, 'section_list._'), 'utf8'),
-    sharedImports
-  );
-  sharedImports.imports.renderSection = _.template(
-    fs.readFileSync(path.join(__dirname, 'section._'), 'utf8'),
-    sharedImports
-  );
-  sharedImports.imports.renderNote = _.template(
-    fs.readFileSync(path.join(__dirname, 'note._'), 'utf8'),
-    sharedImports
-  );
-  sharedImports.imports.renderParamProperty = _.template(
-    fs.readFileSync(path.join(__dirname, 'paramProperty._'), 'utf8'),
-    sharedImports
-  );
+  sharedImports.imports.renderSectionList = _.template(fs.readFileSync(path.join(__dirname, 'section_list._'), 'utf8'), sharedImports)
+  sharedImports.imports.renderSection = _.template(fs.readFileSync(path.join(__dirname, 'section._'), 'utf8'), sharedImports)
+  sharedImports.imports.renderNote = _.template(fs.readFileSync(path.join(__dirname, 'note._'), 'utf8'), sharedImports)
+  sharedImports.imports.renderParamProperty = _.template(fs.readFileSync(path.join(__dirname, 'paramProperty._'), 'utf8'), sharedImports)
 
-  var pageTemplate = _.template(
-    fs.readFileSync(path.join(__dirname, 'index._'), 'utf8'),
-    sharedImports
-  );
+  var pageTemplate = _.template(fs.readFileSync(path.join(__dirname, 'index._'), 'utf8'), sharedImports)
 
   // push assets into the pipeline as well.
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     vfs.src([__dirname + '/assets/**'], { base: __dirname }).pipe(
       concat(function (files) {
         resolve(
@@ -118,14 +89,14 @@ module.exports = function (comments, config) {
               contents: new Buffer(
                 pageTemplate({
                   docs: comments,
-                  config
+                  config,
                 }),
-                'utf8'
-              )
-            })
-          )
-        );
-      })
-    );
-  });
-};
+                'utf8',
+              ),
+            }),
+          ),
+        )
+      }),
+    )
+  })
+}
