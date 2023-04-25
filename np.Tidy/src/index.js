ddd726805df0c8dd2598cfbadad83f8a224c42b2
Jonathan Clark <jonathan@clarksonline.me.uk>
2023-04-25 23:49:38 +0100

    ready to release "Tidy Up" v0.4 when an NP bug is fixed

--- np.Tidy/src/index.js
+++ np.Tidy/src/index.js
@@ -2,13 +2,14 @@
 //-----------------------------------------------------------------------------
 // Tidy plugin
 // Jonathan Clark
-// Last updated 7.3.2023 for v0.4.0, @jgclark
+// Last updated 21.4.2023 for v0.4.0, @jgclark
 //-----------------------------------------------------------------------------
 
 // allow changes in plugin.json to trigger recompilation
 import pluginJson from '../plugin.json'
+import { JSP, logDebug, logError, logInfo } from '@helpers/dev'
 import { pluginUpdated, updateSettingData } from '@helpers/NPConfiguration'
-import { JSP, logError, logInfo } from '@helpers/dev'
+import { editSettings } from '@helpers/NPSettings'
 
 const pluginID = 'np.Tidy'
 
@@ -32,3 +33,17 @@ export { onUpdateOrInstall, init, onSettingsUpdated } from './triggers-hooks'
 
 // Note: not yet written or used:
 // export { onOpen, onEditorWillSave } from './NPTriggers-Hooks'
+
+/**
+ * Update Settings/Preferences (for iOS etc)
+ * Plugin entrypoint for command: "/<plugin>: Update Plugin Settings/Preferences"
+ * @author @dwertheimer
+ */
+export async function updateSettings() {
+  try {
+    logDebug(pluginJson, `updateSettings running`)
+    await editSettings(pluginJson)
+  } catch (error) {
+    logError(pluginJson, JSP(error))
+  }
+}
