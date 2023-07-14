4b269ead4fcaa85092a79099dc214a80fe82864c
jgclark <jonathan@clarksonline.me.uk>
2023-07-14 11:30:39 +0100

    Remove unused code

--- helpers/syncedCopies.js
+++ helpers/syncedCopies.js
@@ -1,7 +1,6 @@
 // @flow
 
 import { sortListBy } from '@helpers/sorting'
-// import { displayTitle } from '@helpers/general'
 import { JSP, logDebug, logError } from '@helpers/dev'
 
 // Note: Eduard's regex looks for a trailing space or end of line. I can't use that part because it will remove space we need if
@@ -11,7 +10,8 @@ export const textWithoutSyncedCopyTag = (text: string): string => text.replace(n
 /**
  * Eliminate duplicate paragraphs (especially for synced lines)
  * Duplicate content is not allowed if:
- * - The content is the same & the blockID is the same (multiple notes referencing this one)
+ * - the content is the same
+ * - the blockID is the same (multiple notes referencing this one) if 'syncedLinesOnly' is true
  * By default it keeps the first copy it finds ... so this is dependent on the order of paras passed to the function.
  * But you can select the most recently-changed note's version instead, by passing 'most-recent' as second parameter.
  * @author @dwertheimer updated by @jgclark
@@ -25,35 +25,21 @@ export function eliminateDuplicateSyncedParagraphs(paras: Array<TParagraph>, kee
     // logDebug('eliminateDuplicateSyncedParagraphs', `starting for ${String(paras.length)} paras with ${keepWhich}`)
     const revisedParas = []
     if (paras?.length > 0) {
-      // if (keepWhich === 'most-recent') {
-      // sort the list by most recent changed date
-      // this allows us to still keep the main logic below
-      // sortedParas = sortListBy(paras, ['-changedDate'])
-      // })
-      // } else {
-      //   sortedParas = paras
-      // }
       const sortedParas = keepWhich === 'most-recent' ? sortListBy(paras, ['-note.changedDate']) : paras
-      if (keepWhich === 'most-recent') {
-        sortedParas.map((p) => {
-          const n = p.note
-          // logDebug('eDSP', `- ${p.filename ?? '-'} / ${String(n?.changedDate)} / ${p.content}`)
-        })
-      }
 
       // keep first in list (either way)
       sortedParas.forEach((e) => {
         const matchingIndex = revisedParas.findIndex((t) => {
           if (t.content === e.content) {
             if (t.blockId !== undefined && e.blockId !== undefined && t.blockId === e.blockId) {
-              logDebug('eDSP', `Duplicate line eliminated: "${t.content}" in "${t.filename || ''}" and "${e.filename || ''}"`)
+              logDebug('eDSP', `Duplicate sync line eliminated: "${t.content}" in "${t.filename || ''}" and "${e.filename || ''}"`)
               return true
             } else {
               if (t.filename === e.filename && !syncedLinesOnly) {
-                logDebug('eDSP', `Duplicate line eliminated: "${t.content}" in "${t.filename || ''}" and "${e.filename || ''}"`)
+                logDebug('eDSP', `Duplicate sync line eliminated: "${t.content}" in "${t.filename || ''}" and "${e.filename || ''}"`)
                 return true
               } else {
-                logDebug('eDSP', `Duplicate line allowed due to settings: "${t.content}" in "${t.filename || ''}" and "${e.filename || ''}"`)
+                logDebug('eDSP', `Duplicate sync line allowed: "${t.content}" in "${t.filename || ''}" and "${e.filename || ''}"`)
               }
             }
           }
