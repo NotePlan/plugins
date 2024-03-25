2f4007b3a5493b58cee56e4afe27e58e65a74af9
David Wertheimer <dwertheimer@users.noreply.github.com>
2024-03-25 14:29:50 -0600

    Added useContext to pass functions to communicate w/plugin

--- jgclark.DashboardReact/src/react/components/AddButtons.jsx
+++ jgclark.DashboardReact/src/react/components/AddButtons.jsx
@@ -2,58 +2,56 @@
 // @flow
 
 import React from 'react'
+import { useAppContext } from './AppContext.jsx'
 
-type Props = {
-    sendActionToPlugin: (command: string, dataToSend: any) => void /* sends a command and data to the plugin */,
-}
+const AddButtons = (): React$Node => {
+  const { sendActionToPlugin /*, sendToPlugin, dispatch, pluginData */ } = useAppContext()
 
-const AddButtons = ({ sendActionToPlugin }: Props): React$Node => {
-
-    // this is just cut and paste for now, needs to be refactored to use Button/React
-return (
+  // this is just cut and paste for now, needs to be refactored to use Button/React
+  return (
     <>
-    <button
-    className="XCBButton tooltip"
-    data-tooltip="Add a new task to today's note"
-    data-plugin-id="jgclark.Dashboard"
-    data-command="addTask"
-    data-command-args="20240324.md"
-    onClick={()=>sendActionToPlugin('addTask', '20240324.md')}
-  >
-    <i className="fa-regular fa-circle-plus sidebarDaily"></i>
-  </button>
-  <button
-    className="XCBButton tooltip"
-    data-tooltip="Add a new checklist to today's note"
-    data-plugin-id="jgclark.Dashboard"
-    data-command="addChecklist"
-    data-command-args="20240324.md"
-    onClick={()=>sendActionToPlugin('addChecklist', '20240324.md')}
-  >
-    <i className="fa-regular fa-square-plus sidebarDaily"></i>
-  </button>{' '}
-  <button
-    className="XCBButton tooltip"
-    data-tooltip="Add a new task to tomorrow's note"
-    data-plugin-id="jgclark.Dashboard"
-    data-command="addTask"
-    data-command-args="20240325.md"
-    onClick={()=>sendActionToPlugin('addTask', '20240325.md')}
-  >
-    <i className="fa-regular fa-circle-arrow-right sidebarDaily"></i>
-  </button>
-  {' '}
-  <button
-    className="XCBButton tooltip"
-    data-tooltip="Add a new checklist to tomorrow's note"
-    data-plugin-id="jgclark.Dashboard"
-    data-command="addChecklist"
-    data-command-args="20240325.md"
-    onClick={()=>sendActionToPlugin('addChecklist', '20240325.md')}
-  >
-    <i className="fa-regular fa-square-arrow-right sidebarDaily"></i>
-  </button></>
-)
+      <button
+        className="XCBButton tooltip"
+        data-tooltip="Add a new task to today's note"
+        data-plugin-id="jgclark.Dashboard"
+        data-command="addTask"
+        data-command-args="20240324.md"
+        onClick={() => sendActionToPlugin('addTask', '20240324.md')}
+      >
+        <i className="fa-regular fa-circle-plus sidebarDaily"></i>
+      </button>
+      <button
+        className="XCBButton tooltip"
+        data-tooltip="Add a new checklist to today's note"
+        data-plugin-id="jgclark.Dashboard"
+        data-command="addChecklist"
+        data-command-args="20240324.md"
+        onClick={() => sendActionToPlugin('addChecklist', '20240324.md')}
+      >
+        <i className="fa-regular fa-square-plus sidebarDaily"></i>
+      </button>{' '}
+      <button
+        className="XCBButton tooltip"
+        data-tooltip="Add a new task to tomorrow's note"
+        data-plugin-id="jgclark.Dashboard"
+        data-command="addTask"
+        data-command-args="20240325.md"
+        onClick={() => sendActionToPlugin('addTask', '20240325.md')}
+      >
+        <i className="fa-regular fa-circle-arrow-right sidebarDaily"></i>
+      </button>{' '}
+      <button
+        className="XCBButton tooltip"
+        data-tooltip="Add a new checklist to tomorrow's note"
+        data-plugin-id="jgclark.Dashboard"
+        data-command="addChecklist"
+        data-command-args="20240325.md"
+        onClick={() => sendActionToPlugin('addChecklist', '20240325.md')}
+      >
+        <i className="fa-regular fa-square-arrow-right sidebarDaily"></i>
+      </button>
+    </>
+  )
 }
 
 export default AddButtons
--- jgclark.DashboardReact/src/react/components/Dashboard.jsx
+++ jgclark.DashboardReact/src/react/components/Dashboard.jsx
@@ -2,30 +2,26 @@
 import React from 'react'
 import Header from './Header.jsx'
 import Section from './Section.jsx'
+// import { useAppContext } from './AppContext.jsx'
 
 type Props = {
-  data: Object /* the data that was sent from the plugin in the field "pluginData" */,
-  dispatch: number /* sends a message to the HTML window without necessarily sending info to the plugin */,
-  sendActionToPlugin: (command: string, dataToSend: any) => void /* sends a command and data to the plugin */,
+  pluginData: Object /* the data that was sent from the plugin in the field "pluginData" */,
 }
 
 /**
  * Dashboard component aggregating data and layout for the dashboard.
  */
-const Dashboard = ({ data, dispatch, sendActionToPlugin }: Props): React$Node => {
-  const {sections, lastUpdated, totalItems} =  data 
-
-  const refreshHandler = () => {
-    sendActionToPlugin('refresh', {})
-  }
+const Dashboard = ({pluginData}:Props): React$Node => {
+//   const { sendActionToPlugin, sendToPlugin, dispatch, pluginData }  = useAppContext()
+  const {sections, lastUpdated} =  pluginData 
 
   return (
     <div style={{ maxWidth: '100vw', width: '100vw' }}>
       <div className="dashboard">
-        <Header lastUpdated={lastUpdated} totalItems={totalItems} refreshHandler={refreshHandler} />
+        <Header lastUpdated={lastUpdated} />
         {/* Assuming sections data is fetched or defined elsewhere and passed as props */}
         {sections.map((section, index) => (
-          <Section key={index} {...section} sendActionToPlugin={sendActionToPlugin}/>
+          <Section key={index} {...section} />
         ))}
       </div>
     </div>
