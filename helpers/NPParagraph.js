0cf8fa5f79df9a30b0318463ea9904e5852878e9
Jonathan Clark <jonathan@clarksonline.me.uk>
2024-08-31 22:19:11 +0100

    Add removeAllDueDates()

--- helpers/NPParagraph.js
+++ helpers/NPParagraph.js
@@ -30,7 +30,7 @@ import { findStartOfActivePartOfNote, isTermInMarkdownPath, isTermInURL, smartPr
 import { RE_FIRST_SCHEDULED_DATE_CAPTURE } from '@helpers/regex'
 import { getLineMainContentPos } from '@helpers/search'
 import { stripTodaysDateRefsFromString } from '@helpers/stringTransforms'
-import { hasScheduledDate, isOpen } from '@helpers/utils'
+import { hasScheduledDate, isOpen, isOpenAndScheduled } from '@helpers/utils'
 // import { showMessageYesNoCancel } from '@helpers/userInput'
 
 const pluginJson = 'NPParagraph'
@@ -1752,12 +1752,13 @@ export function unscheduleItem(filename: string, content: string): boolean {
     // Get the paragraph to change
     const thisPara = possiblePara
     const thisNote = thisPara.note
+    if (!thisNote) throw new Error(`Could not get note for filename ${filename}`)
+
     // Find and then remove any scheduled dates
     const thisLine = possiblePara.content
     logDebug('unscheduleItem', `unscheduleItem('${thisLine}'`)
     thisPara.content = replaceArrowDatesInString(thisLine, '')
     logDebug('unscheduleItem', `unscheduleItem('${thisPara.content}'`)
-    // $FlowIgnore(incompatible-use)
     thisNote.updateParagraph(thisPara)
     return true
   } catch (error) {
@@ -1894,3 +1895,34 @@ export const getOpenTasksAndChildren = (paragraphs: Array<TParagraph>): Array<TP
       .map((p) => [p.lineIndex, p]), // Map each paragraph to a [lineIndex, paragraph] pair
   ).values(),
 ] // Extract the values (unique paragraphs) from the Map and spread into an array
+
+/**
+ * Remove all due dates from a project note given by filename.
+ * Called by togglePauseProject
+ * @param {string} filename to work on
+ */
+export function removeAllDueDates(filename: string): boolean {
+  try {
+    logDebug('removeAllDueDates', `Starting for ${filename} ...`)
+    const note = DataStore.projectNoteByFilename(filename)
+    if (!note) {
+      throw new Error(`Couldn't find note for ${filename}`)
+    }
+    const paras = note.paragraphs.filter((para) => isOpenAndScheduled(para))
+    logDebug('removeAllDueDates', `- will remove scheduled dates from ${String(paras.length)} paras...`)
+    // remove all >dates, of type "scheduled" or "open" and checklist equivalents
+    paras.forEach((para) => {
+      const thisLine = para.content
+      para.content = replaceArrowDatesInString(thisLine, '')
+      if (para.type === 'scheduled') para.type = 'open'
+      if (para.type === 'checklistScheduled') para.type = 'checklist'
+    })
+    note.updateParagraphs(paras)
+    logDebug('removeAllDueDates', `- this appears to have worked.`)
+    return true
+  }
+  catch (error) {
+    logError('removeAllDueDates', error.message)
+    return false
+  }
+}
