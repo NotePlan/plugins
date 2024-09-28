1bf84aacf4d89c488bc446b61f05b48f62aa0107
jgclark <jonathan@clarksonline.me.uk>
2024-09-28 14:58:06 +0100

    Update Dashboard for PROJECT to use newer allProjects structure

--- helpers/general.js
+++ helpers/general.js
@@ -210,6 +210,7 @@ export function createOpenOrDeleteNoteCallbackUrl(
   const head = heading && heading.length ? encodePlusParens(heading.replace('#', '')) : ''
   // console.log(`createOpenOrDeleteNoteCallbackUrl: ${xcb}${titleOrFilename}${head ? `&heading=${head}` : ''}`)
   const encodedTitleOrFilename = encodePlusParens(titleOrFilename)
+  logDebug('createOpenOrDeleteNoteCallbackUrl', `encodePlusParens -> '${encodedTitleOrFilename}'`)
   const openAs = openType && ['subWindow', 'splitView', 'useExistingSubWindow'].includes(openType) ? `&${openType}=yes` : ''
   let retVal = ''
   if (isLineLink) {
