c2cf7122091be1f18d0e4c3748d41cfab2b3a3ca
David Wertheimer <dwertheimer@users.noreply.github.com>
2024-11-20 17:35:55 -0800

    fix babel to work for both jest and rollup for app

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,19 @@
 // babel.config.js
-module.exports = {
-  presets: [
-    '@babel/preset-flow',
-    '@babel/preset-react',
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
+module.exports = (api) => {
+  const isTest = api.env('test')
+  return {
+    presets: [
+      '@babel/preset-flow',
+      '@babel/preset-react',
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+          modules: isTest ? 'commonjs' : false, // Use CommonJS for Jest, ES modules for Rollup
         },
-        modules: 'commonjs', // Ensure modules are transformed to CommonJS
-      },
+      ],
     ],
-  ],
+  }
 }
