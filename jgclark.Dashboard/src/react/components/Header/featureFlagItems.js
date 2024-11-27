b2fae109c9c9f450dd94af3512bcf1a09a75c815
David Wertheimer <dwertheimer@users.noreply.github.com>
2024-11-26 22:51:52 -0600

    forgot a file

--- jgclark.Dashboard/src/react/components/Header/featureFlagItems.js
+++ jgclark.Dashboard/src/react/components/Header/featureFlagItems.js
@@ -10,7 +10,8 @@ const featureFlagSettingDefs = [
     description: 'Rather than incremental section loading, force full initial load. Mostly useful for testing full data in a browser.',
   },
   { key: 'FFlag_HardRefreshButton', label: 'Show Hard Refresh Button', description: 'Show button that does a full window reload with changed React components and data' },
-  { key: 'FFlag_DebugPanel', label: 'Show Debug Pane', description: 'Show debug pane with test runner and console log viewer at the bottom of the page' },
+  { key: 'FFlag_DebugPanel', label: 'Show Debug Panel', description: 'Show debug pane with test runner and console log viewer at the bottom of the page' },
+  { key: 'FFlag_ShowTestingPanel', label: 'Show Testing Pane', description: 'Show testing panel with end-to-end testing buttons (requires Debug Panel)' },
 ]
 
 export const createFeatureFlagItems = (dashboardSettings: TDashboardSettings): Array<TSettingItem> => {
