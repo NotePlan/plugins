6299562d364c23970d6fb3f715c44ff063856729
David Wertheimer <dwertheimer@users.noreply.github.com>
2024-12-08 19:22:35 -0800

    More flow improvements

--- np.Shared/src/react/DynamicDialog/dialogElementRenderer.js
+++ np.Shared/src/react/DynamicDialog/dialogElementRenderer.js
@@ -149,23 +149,12 @@ export function renderItem({
           <ThemedSelect
             disabled={disabled}
             key={`cmb${index}`}
-            options={
-              item.options
-                ? item.options.map((option) => {
-                    if (typeof option === 'string') {
-                      return { label: option, value: option, isDefault: false }
-                    }
-                    return option
-                  })
-                : []
-            }
+            options={item.options ? item.options.map((option) => (typeof option === 'string' ? { label: option, value: option } : option)) : []} // Normalize options to ensure they are in { label, value } format
             value={item.value || item.default || undefined} // Ensure value is not undefined
             onChange={(selectedOption) => {
-              if (selectedOption && typeof selectedOption.value === 'string') {
-                const value = selectedOption.value
-                item.key && handleFieldChange(item.key, value)
-                item.key && handleComboChange(item.key, value)
-              }
+              const value = selectedOption ? selectedOption.value : null // Get the value from the selected option
+              item.key && handleFieldChange(item.key, value)
+              item.key && handleComboChange(item.key, value) // Pass the selected option
             }}
             inputRef={inputRef} // Pass inputRef
             compactDisplay={item.compactDisplay || false}
@@ -307,7 +296,7 @@ export function renderItem({
         const selectedDate = item.selectedDate || null
         const numberOfMonths = item.numberOfMonths || 1
 
-        const handleDateChange = (date: Date) => {
+        const handleDateChange = (date) => {
           if (item.key) {
             handleFieldChange(item.key, date)
           }
