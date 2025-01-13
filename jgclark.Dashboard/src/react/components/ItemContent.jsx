1b05e00acfd80b3bad35017558e904c5ddaa8b61
Jonathan Clark <jonathan@clarksonline.me.uk>
2025-01-13 04:51:17 +0000

    Fix spacing after folder names in referenced links

--- jgclark.Dashboard/src/react/components/ItemContent.jsx
+++ jgclark.Dashboard/src/react/components/ItemContent.jsx
@@ -91,8 +91,8 @@ function ItemContent({ item /*, children */, thisSection }: Props): React$Node {
   // const possParentIcon = dashboardSettings.parentChildMarkersEnabled && item.para?.hasChild ? <i className="fa-regular fa-block-quote parentMarker pad-left"></i> : ''
   // v3: switch to ellipsis to match what main Editor has just got in 3.15.2
   const possParentIcon = dashboardSettings.parentChildMarkersEnabled && item.para?.hasChild ? <i className="fa-solid fa-ellipsis parentMarker"></i> : ''
-  // TODO: add childID and only display ... if it has a displayed child
   
+  // Note: this section now deliberately disabled
   // if isAChild, then set suitable icon (previously tried arrow-right-from-line)
   // Note: now handled by flex layout and indent on ItemRow
   // Note: Following only for debugging
