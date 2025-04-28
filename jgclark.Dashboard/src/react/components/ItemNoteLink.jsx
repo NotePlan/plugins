f6240f8ba4e9869f7ecc46f1623939f0b7dc6be3
jgclark <jonathan@clarksonline.me.uk>
2025-04-28 22:17:44 +0100

    Merge tagMentionCache into main

--- jgclark.Dashboard/src/react/components/ItemNoteLink.jsx
+++ jgclark.Dashboard/src/react/components/ItemNoteLink.jsx
@@ -1,7 +1,7 @@
 // @flow
 //--------------------------------------------------------------------------
 // Dashboard React component to show Note Links after main item content
-// Last updated 2025-01-13 for v2.1.2 by @jgclark
+// Last updated 2025-04-22 for v2.2.2 by @jgclark
 //--------------------------------------------------------------------------
 import React from 'react'
 import type { TSection, TSectionItem } from '../../types.js'
@@ -13,19 +13,22 @@ import { extractModifierKeys } from '@helpers/react/reactMouseKeyboard.js'
 
 type Props = {
   item: TSectionItem,
-  thisSection: TSection,
+  // thisSection: TSection,
 }
 
 /**
  * Represents the main content for a single item within a section
  */
-function ItemNoteLink({ item, thisSection }: Props): React$Node {
+// function ItemNoteLink({ item, thisSection }: Props): React$Node {
+function ItemNoteLink({ item }: Props): React$Node {
   const { sendActionToPlugin, dashboardSettings, reactSettings } = useAppContext()
   const filename = item.para?.filename ?? '<no filename found>'
   // compute the things we need later
   const noteTitle = item?.para?.title || ''
   // logDebug(`ItemNoteLink`, `ItemNoteLink for item.itemFilename:${filename} noteTitle:${noteTitle} thisSection.sectionFilename=${thisSection.sectionFilename || ''}`)
+  logDebug(`ItemNoteLink`, `ItemNoteLink for item.itemFilename:${filename} noteTitle:${noteTitle}`)
   const folderNamePart = dashboardSettings.includeFolderName && filename !== '<no filename found>' && getFolderFromFilename(filename) !== '/' ? `${getFolderFromFilename(filename)} /` : ''
+
   const handleLinkClick = (e:MouseEvent) => {
     const { modifierName  } = extractModifierKeys(e) // Indicates whether a modifier key was pressed
 
@@ -37,23 +40,27 @@ function ItemNoteLink({ item, thisSection }: Props): React$Node {
     sendActionToPlugin(dataObjectToPassToFunction.actionType, dataObjectToPassToFunction, `${noteTitle} clicked`, true)
   }
 
-  if (filename !== thisSection.sectionFilename) {
+  const iconToUse = item.para?.isTeamspace ? 'fa-regular fa-screen-users' : 'fa-regular fa-file-lines'
+  const linkClass = item.para?.isTeamspace ? 'teamspaceName' : 'noteTitle'
+  const linkStyle = item.para?.isTeamspace ? 'teamspaceName' : 'folderName'
+
+  // if (filename !== thisSection.sectionFilename) {
     return (
       <TooltipOnKeyPress
         altKey={{ text: 'Open in Split View' }}
         metaKey={{ text: 'Open in Floating Window' }}
         label={`${item.itemType}_${item.ID}_Open Note Link`}
         enabled={!reactSettings?.dialogData?.isOpen}>
-        <span className="pad-left-larger folderName pad-right">{folderNamePart}</span>
-        <a className="noteTitle sectionItem" onClick={handleLinkClick}>
-          <i className="fa-regular fa-file-lines pad-right"></i>
+        <span className={`pad-left-larger ${linkStyle} pad-right`}>{folderNamePart}</span>
+        <a className={`${linkClass} ${linkStyle} sectionItem`} onClick={handleLinkClick}>
+          <i className={`${iconToUse} pad-right`}></i>
           {noteTitle}
         </a>
       </TooltipOnKeyPress>
     )
-  } else {
-    return null
-  }
+  // } else {
+  //   return null
+  // }
 }
 
 export default ItemNoteLink
