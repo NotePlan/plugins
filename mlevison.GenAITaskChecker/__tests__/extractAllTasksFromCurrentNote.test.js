d58b4e9a14bd7f497293fbfd79e6ccb2718ed6a4
Mark Levison <mark@agilepainrelief.com>
2025-07-21 17:04:58 -0400

    Revert "Add limited version of getAllTasksForCurrentNote"
    This reverts commit 9cf6d954e36c9302ebfe2ebd8b19ee0bb5db3302.

--- mlevison.GenAITaskChecker/__tests__/extractAllTasksFromCurrentNote.test.js
+++ mlevison.GenAITaskChecker/__tests__/extractAllTasksFromCurrentNote.test.js
@@ -1,7 +1,6 @@
 /* global jest, describe, test, expect, beforeAll, afterAll, beforeEach, afterEach */
 import { _ } from 'lodash'
-import { getAllTasksFromCurrentNote } from '../src/support/tasks.js'
-import * as sorting from '@helpers/sorting'
+import * as sorting from '../../helpers/sorting'
 import { CustomConsole, LogType, LogMessage } from '@jest/console' // see note below
 import { Calendar, Clipboard, CommandBar, DataStore, Editor, NotePlan, simpleFormatter, Paragraph /* Note, mockWasCalledWithString, Paragraph */ } from '@mocks/index'
 
@@ -14,15 +13,15 @@ beforeAll(() => {
   global.Editor = Editor
   global.NotePlan = NotePlan
   global.console = new CustomConsole(process.stdout, process.stderr, simpleFormatter) // minimize log footprint
-  DataStore.settings['_logLevel'] = 'none' //change this to DEBUG to get more logging (or 'none' for none)
+  DataStore.settings['_logLevel'] = 'DEBUG' //change this to DEBUG to get more logging (or 'none' for none)
 })
 
 describe('mlevison.GenAITaskChecker', () => {
-  describe('getAllTasks using getTasksByType', () => {
-      test('prove Open and Cancelled Tasks found in sample note', () => {
-          const noteWIthOpenAndCancelledTasks = {
+  describe('getAllTasks', () => {
+      test('prove All Tasks found in sample note', () => {
+          const noteA = {
             paragraphs: [
-              { type: 'title', lineIndex: 0, content: 'Title', headingLevel: 1 },
+              { type: 'title', lineIndex: 0, content: 'NoteA Title', headingLevel: 1 },
               { type: 'empty', lineIndex: 1 },
               { type: 'title', lineIndex: 2, content: 'Tasks for 3.4.22', headingLevel: 2 },
               { type: 'open', lineIndex: 3, content: 'task 1' },
@@ -35,66 +34,13 @@ describe('mlevison.GenAITaskChecker', () => {
               { type: 'cancelled', lineIndex: 10, content: 'task 4 not done' },
             ],
           }
-        const foundTasks = sorting.getTasksByType(noteWIthOpenAndCancelledTasks.paragraphs)
+        const foundTasks = sorting.getTasksByType(noteA.paragraphs)
 
         console.log('foundTasks open', foundTasks.open, foundTasks.done, foundTasks.cancelled)
          
         expect(foundTasks.open.length).toBe(1)
         expect(foundTasks.done.length).toBe(0)
         expect(foundTasks.cancelled.length).toBe(1)
-        expect(foundTasks.scheduled.length).toBe(0)
-        expect(foundTasks.checklist.length).toBe(0)
-        expect(foundTasks.checklistDone.length).toBe(0)
-        expect(foundTasks.checklistCancelled.length).toBe(0)
-        expect(foundTasks.checklistScheduled.length).toBe(0)
       })
-
-      test('prove Done Tasks found in sample note', () => {
-          const noteWIthDoneAndCancelledTasks = {
-            paragraphs: [
-              { type: 'title', lineIndex: 0, content: 'Title', headingLevel: 1 },
-              { type: 'empty', lineIndex: 1 },
-              { type: 'title', lineIndex: 2, content: 'Tasks for 3.4.22', headingLevel: 2 },
-              { type: 'scheduled', lineIndex: 3, content: 'task 1' },
-              { type: 'title', lineIndex: 4, content: 'Journal for 3.4.22' },
-              { type: 'list', lineIndex: 5, content: 'first journal entry' },
-              { type: 'list', lineIndex: 6, content: 'second journal entry' },
-              { type: 'empty', lineIndex: 7 },
-              { type: 'title', lineIndex: 8, content: 'Done ...', headingLevel: 2 },
-              { type: 'done', lineIndex: 10, content: 'task finised' }, 
-            ]
-          }
-        const foundTasks = sorting.getTasksByType(noteWIthDoneAndCancelledTasks.paragraphs)
-
-        console.log('foundTasks open', foundTasks.open, foundTasks.done, foundTasks.cancelled)
-         
-        expect(foundTasks.open.length).toBe(0)
-        expect(foundTasks.done.length).toBe(1)
-        expect(foundTasks.cancelled.length).toBe(0)
-        expect(foundTasks.scheduled.length).toBe(1)
-        expect(foundTasks.checklist.length).toBe(0)
-        expect(foundTasks.checklistDone.length).toBe(0)
-        expect(foundTasks.checklistCancelled.length).toBe(0)
-        expect(foundTasks.checklistScheduled.length).toBe(0)
-      })
-  })
-
-  describe('getAllTasksFromCurrentNote', () => {
-    test('returns open tasks from current note', () => {
-      const currentNote = {
-        paragraphs: [
-          { type: 'title', lineIndex: 0, content: 'Title', headingLevel: 1 },
-          { type: 'empty', lineIndex: 1 },
-          { type: 'open', lineIndex: 2, content: 'Open task 1' },
-          { type: 'open', lineIndex: 3, content: 'Open task 2' },
-          { type: 'done', lineIndex: 4, content: 'Done task' },
-        ],
-      }
-      const tasks = getAllTasksFromCurrentNote(currentNote)
-      expect(tasks.length).toBe(3)
-      expect(tasks[0].content).toBe('Open task 1')
-      expect(tasks[1].content).toBe('Open task 2')
-      expect(tasks[2].content).toBe('Done task')
-    })
   })
 })
