787a7eb35c673d84c84557c70974d5ef77af9e7d
jgclark <jonathan@clarksonline.me.uk>
2025-08-14 09:02:30 +0100

    More updates to folder helpers to support Teamspaces

--- helpers/__tests__/teamspace.test.js
+++ helpers/__tests__/teamspace.test.js
@@ -22,7 +22,33 @@ const PLUGIN_NAME = `📙 ${colors.yellow('helpers/teamspace')}`
 // const section = colors.blue
 
 describe(`${PLUGIN_NAME}`, () => {
-  /*
+  /**
+   * isTeamspaceNoteFromFilename()
+   */
+  describe('isTeamspaceNoteFromFilename()' /* function */, () => {
+    test('should return true for a Teamspace calendar filename', () => {
+      const result = t.isTeamspaceNoteFromFilename('%%NotePlanCloud%%/c484b190-77dd-4d40-a05c-e7d7144f24e1/20250422.md')
+      expect(result).toEqual(true)
+    })
+    test('should return false for a non-Teamspace calendar filename', () => {
+      const result = t.isTeamspaceNoteFromFilename('20250422.md')
+      expect(result).toEqual(false)
+    })
+    test('should return false for a non-Teamspace filename', () => {
+      const result = t.isTeamspaceNoteFromFilename('TEST/teamspace testing.md')
+      expect(result).toEqual(false)
+    })
+    test('should return true for a Teamspace regular note filename', () => {
+      const result = t.isTeamspaceNoteFromFilename('%%NotePlanCloud%%/c484b190-77dd-4d40-a05c-e7d7144f24e1/5a31e9ea-732f-45ba-8464-11260522e0de')
+      expect(result).toEqual(true)
+    })
+    test('should return true for a Teamspace sub-folder regular note filename', () => {
+      const result = t.isTeamspaceNoteFromFilename('%%NotePlanCloud%%/c484b190-77dd-4d40-a05c-e7d7144f24e1/test folder/5a31e9ea-732f-45ba-8464-11260522e0de')
+      expect(result).toEqual(true)
+    })
+  })
+
+  /**
    * parseTeamspaceFilename()
    */
   describe('parseTeamspaceFilename()' /* function */, () => {
@@ -61,6 +87,13 @@ describe(`${PLUGIN_NAME}`, () => {
       expect(result.filepath).toEqual('Dashboard Issues')
       expect(result.teamspaceID).toEqual('1b91b194-4c76-4a48-8d4d-4c499d64a919')
     })
+    test('should parse a Teamspace folder path', () => {
+      const result = t.parseTeamspaceFilename('%%NotePlanCloud%%/1b91b194-4c76-4a48-8d4d-4c499d64a919/Dashboard Issues')
+      expect(result.isTeamspace).toEqual(true)
+      expect(result.filename).toEqual('Dashboard Issues')
+      expect(result.filepath).toEqual('Dashboard Issues')
+      expect(result.teamspaceID).toEqual('1b91b194-4c76-4a48-8d4d-4c499d64a919')
+    })
   })
 
   /*
@@ -86,6 +119,24 @@ describe(`${PLUGIN_NAME}`, () => {
     test('should parse a Teamspace regular note filename in a folder', () => {
       const result = t.getFilenameWithoutTeamspaceID('%%NotePlanCloud%%/1b91b194-4c76-4a48-8d4d-4c499d64a919/Dashboard Issues/9972af6a-ec7a-4fe5-87b9-9005aa0d122c')
       expect(result).toEqual('Dashboard Issues/9972af6a-ec7a-4fe5-87b9-9005aa0d122c')
+    })
+  })
+
+  /*
+   * getTeamspaceIDFromFilename()
+   */
+  describe('getTeamspaceIDFromFilename()' /* function */, () => {
+    test('should parse a non-teamspace calendar filename', () => {
+      const result = t.getTeamspaceIDFromFilename('20250422.md')
+      expect(result).toEqual('')
+    })
+    test('should parse a non-teamspace filename', () => {
+      const result = t.getTeamspaceIDFromFilename('TEST/teamspace testing.md')
+      expect(result).toEqual('')
+    })
+    test('should parse a Teamspace calendar filename', () => {
+      const result = t.getTeamspaceIDFromFilename('%%NotePlanCloud%%/c484b190-77dd-4d40-a05c-e7d7144f24e1/20250422.md')
+      expect(result).toEqual('c484b190-77dd-4d40-a05c-e7d7144f24e1')
     })
   })
 })
