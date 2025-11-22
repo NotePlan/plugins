bc0757ad76d82690883c1e4ed3690889d9691b3a
Jonathan Clark <jonathan@clarksonline.me.uk>
2025-11-22 22:56:51 +0000

    Fix broken test in jgclark.EventHelpers

--- jgclark.EventHelpers/__tests__/eventsToNotes.test.js
+++ jgclark.EventHelpers/__tests__/eventsToNotes.test.js
@@ -17,21 +17,21 @@ beforeAll(() => {
 
 describe('eventsToNotes.js tests', () => {
   describe('sortByCalendarNameThenStartTime() using HH:MM-strings as times', () => {
-    const mapForSorting: { cal: string, start: string, text: string }[] = []
-    mapForSorting.push({ cal: 'calB', start: '09:00', text: 'event string 1' })
-    mapForSorting.push({ cal: 'calA', start: '10:00', text: 'event string 2' })
-    mapForSorting.push({ cal: 'calC', start: '11:00', text: 'event string 3' })
-    mapForSorting.push({ cal: 'calB', start: '11:00', text: 'event string 4' })
-    mapForSorting.push({ cal: 'calA', start: '23:00', text: 'event string 5' })
-    mapForSorting.push({ cal: 'calC', start: '00:00', text: 'event string 6' })
+    const mapForSorting: { cal: string, start: Date, text: string }[] = []
+    mapForSorting.push({ cal: 'calB', start: new Date(2021, 0, 1, 9, 0, 0), text: 'event string 1' })
+    mapForSorting.push({ cal: 'calA', start: new Date(2021, 0, 1, 10, 0, 0), text: 'event string 2' })
+    mapForSorting.push({ cal: 'calC', start: new Date(2021, 0, 1, 11, 0, 0), text: 'event string 3' })
+    mapForSorting.push({ cal: 'calB', start: new Date(2021, 0, 1, 11, 0, 0), text: 'event string 4' })
+    mapForSorting.push({ cal: 'calA', start: new Date(2021, 0, 1, 23, 0, 0), text: 'event string 5' })
+    mapForSorting.push({ cal: 'calC', start: new Date(2021, 0, 1, 0, 0, 0), text: 'event string 6' })
 
-    const sortedMap: { cal: string, start: string, text: string }[] = []
-    sortedMap.push({ cal: 'calA', start: '10:00', text: 'event string 2' })
-    sortedMap.push({ cal: 'calA', start: '23:00', text: 'event string 5' })
-    sortedMap.push({ cal: 'calB', start: '09:00', text: 'event string 1' })
-    sortedMap.push({ cal: 'calB', start: '11:00', text: 'event string 4' })
-    sortedMap.push({ cal: 'calC', start: '00:00', text: 'event string 6' })
-    sortedMap.push({ cal: 'calC', start: '11:00', text: 'event string 3' })
+    const sortedMap: { cal: string, start: Date, text: string }[] = []
+    sortedMap.push({ cal: 'calA', start: new Date(2021, 0, 1, 10, 0, 0), text: 'event string 2' })
+    sortedMap.push({ cal: 'calA', start: new Date(2021, 0, 1, 23, 0, 0), text: 'event string 5' })
+    sortedMap.push({ cal: 'calB', start: new Date(2021, 0, 1, 9, 0, 0), text: 'event string 1' })
+    sortedMap.push({ cal: 'calB', start: new Date(2021, 0, 1, 11, 0, 0), text: 'event string 4' })
+    sortedMap.push({ cal: 'calC', start: new Date(2021, 0, 1, 0, 0, 0), text: 'event string 6' })
+    sortedMap.push({ cal: 'calC', start: new Date(2021, 0, 1, 11, 0, 0), text: 'event string 3' })
 
     test('should sort by calendar name then start time test for HH:MM style times', () => {
       const result = mapForSorting.sort(e.sortByCalendarNameThenStartTime())
